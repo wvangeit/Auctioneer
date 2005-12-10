17b663bb828080bab64f2ba1bc917b672460c354
norganna <norganna@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2005-12-10 02:50:41 +0000

    Add BandAid fix
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@619 21a0ffd4-88d5-4261-b61c-461777ffcffb

---
+++ bandaid/BandAid.lua
@@ -0,0 +1,27 @@
+--[[
+  AddOn that forces load of Blizzard AddOns for those that cannot support it themselves.
+  <%version%>
+		
+	License:
+		This program is free software; you can redistribute it and/or
+		modify it under the terms of the GNU General Public License
+		as published by the Free Software Foundation; either version 2
+		of the License, or (at your option) any later version.
+
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with this program(see GLP.txt); if not, write to the Free Software
+		Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+]]
+
+function BandAid_OnLoad()
+	if (DEFAULT_CHAT_FRAME) then
+		DEFAULT_CHAT_FRAME:AddMessage("BandAid fix loaded. Your Blizzard addons are loaded.", 1, 0.9, 0.2, 1);
+	end
+end
+
