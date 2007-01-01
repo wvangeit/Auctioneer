dce9f835f11a466b4a6b43a254b2e7c0abb165b7
malivil <malivil@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2007-01-01 20:08:40 +0000

    (fixes #1274)Fixes the bug concerning combining stacks of an item while disenchanting.
    I traced the bug and found out that pickupContainerItemHook and pickupInventoryItemHook were still being called while the player was disenchanting. This caused the saved item (the item that enchantrix assumed was being disenchanted) to be changed to whatever was picked up. I added an if to both of those functions so that they are not called when the player is casting a spell, IE Disenchant. I tested this a few times, and it works for me, both when picking up and item and not. Feel free to test it more.
    
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@1281 21a0ffd4-88d5-4261-b61c-461777ffcffb

---
+++ Enchantrix.lua
@@ -0,0 +1,199 @@
+--[[
+
+	Enchantrix v3.9.0.1272 (Kangaroo)
+	$Id: Enchantrix.lua 1194 2006-12-13 08:09:22Z normal $
+
+	By Norganna
+	http://enchantrix.org/
+
+	This is an addon for World of Warcraft that add a list of what an item
+	disenchants into to the items that you mouse-over in the game.
+
+	License:
+		This program is free software; you can redistribute it and/or
+		modify it under the terms of the GNU General Public License
+		as published by the Free Software Foundation; either version 2
+		of the License, or (at your option) any later version.
+
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with this program(see GLP.txt); if not, write to the Free Software
+		Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+	Note:
+		This AddOn's source code is specifically designed to work with
+		World of Warcraft's interpreted AddOn system.
+		You have an implicit licence to use this AddOn with these facilities
+		since that is it's designated purpose as per:
+		http://www.fsf.org/licensing/licenses/gpl-faq.html#InterpreterIncompat
+
+]]
+
+-- Local functions
+local addonLoaded
+local onLoad
+local pickupInventoryItemHook
+local pickupContainerItemHook
+local onEvent
+
+Enchantrix.Version = "3.9.0.1273"
+if (Enchantrix.Version == "<".."%version%>") then
+	Enchantrix.Version = "3.9.DEV"
+end
+
+local DisenchantEvent = {}
+
+-- This function differs from onLoad in that it is executed
+-- after variables have been loaded.
+function addonLoaded(hookArgs, event, addOnName)
+	if (event ~= "ADDON_LOADED") or (addOnName:lower() ~= "enchantrix") then
+		return
+	end
+	Stubby.UnregisterEventHook("ADDON_LOADED", "Enchantrix")
+
+	-- Call AddonLoaded for other objects
+	Enchantrix.Storage.AddonLoaded() -- Sets up saved variables so should be called first
+	Enchantrix.Barker.AddonLoaded()
+	Enchantrix.Command.AddonLoaded()
+	Enchantrix.Config.AddonLoaded()
+	Enchantrix.Locale.AddonLoaded()
+	Enchantrix.Tooltip.AddonLoaded()
+
+	Enchantrix.Revision = Enchantrix.Util.GetRevision("$Revision: 1194 $")
+	for name, obj in pairs(Enchantrix) do
+		if type(obj) == "table" then
+			Enchantrix.Revision = math.max(Enchantrix.Revision, Enchantrix.Util.GetRevision(obj.Revision))
+		end
+	end
+
+	Stubby.RegisterAddOnHook("Auctioneer", "Enchantrix", Enchantrix.Command.AuctioneerLoaded);
+
+	-- Register disenchant detection hooks (using secure post hooks)
+	hooksecurefunc("PickupContainerItem", pickupContainerItemHook)
+	hooksecurefunc("PickupInventoryItem", pickupInventoryItemHook)
+
+	Stubby.RegisterEventHook("UNIT_SPELLCAST_SUCCEEDED", "Enchantrix", onEvent)
+	Stubby.RegisterEventHook("UNIT_SPELLCAST_SENT", "Enchantrix", onEvent)
+	Stubby.RegisterEventHook("UNIT_SPELLCAST_FAILED", "Enchantrix", onEvent)
+	Stubby.RegisterEventHook("UNIT_SPELLCAST_INTERRUPTED", "Enchantrix", onEvent)
+	Stubby.RegisterEventHook("LOOT_OPENED", "Enchantrix", onEvent)
+	Stubby.RegisterEventHook("ZONE_CHANGED", "Enchantrix", onEvent)
+
+	local vstr = ("%s-%d"):format(Enchantrix.Version, Enchantrix.Revision)
+	Enchantrix.Util.ChatPrint(_ENCH('FrmtWelcome'):format(vstr), 0.8, 0.8, 0.2)
+	Enchantrix.Util.ChatPrint(_ENCH('FrmtCredit'), 0.6, 0.6, 0.1)
+end
+
+-- Register our temporary command hook with stubby
+function onLoad()
+	Stubby.RegisterBootCode("Enchantrix", "CommandHandler", [[
+		local function cmdHandler(msg)
+			local i,j, cmd, param = msg:lower():find("^([^ ]+) (.+)$")
+			if (not cmd) then cmd = msg:lower() end
+			if (not cmd) then cmd = "" end
+			if (not param) then param = "" end
+			if (cmd == "load") then
+				if (param == "") then
+					Stubby.Print("Manually loading Enchantrix...")
+					LoadAddOn("Enchantrix")
+				elseif (param == "always") then
+					Stubby.Print("Setting Enchantrix to always load for this character")
+					Stubby.SetConfig("Enchantrix", "LoadType", param)
+					LoadAddOn("Enchantrix")
+				elseif (param == "never") then
+					Stubby.Print("Setting Enchantrix to never load automatically for this character (you may still load manually)")
+					Stubby.SetConfig("Enchantrix", "LoadType", param)
+				else
+					Stubby.Print("Your command was not understood")
+				end
+			else
+				Stubby.Print("Enchantrix is currently not loaded.")
+				Stubby.Print("  You may load it now by typing |cffffffff/enchantrix load|r")
+				Stubby.Print("  You may also set your loading preferences for this character by using the following commands:")
+				Stubby.Print("  |cffffffff/enchantrix load always|r - Enchantrix will always load for this character")
+				Stubby.Print("  |cffffffff/enchantrix load never|r - Enchantrix will never load automatically for this character (you may still load it manually)")
+			end
+		end
+		SLASH_ENCHANTRIX1 = "/enchantrix"
+		SLASH_ENCHANTRIX2 = "/enchant"
+		SLASH_ENCHANTRIX3 = "/enx"
+		SlashCmdList["ENCHANTRIX"] = cmdHandler
+	]]);
+
+	Stubby.RegisterBootCode("Enchantrix", "Triggers", [[
+		if Stubby.GetConfig("Enchantrix", "LoadType") == "always" then
+			LoadAddOn("Enchantrix")
+		else
+			Stubby.Print("]].._ENCH('MesgNotloaded')..[[")
+		end
+	]]);
+	
+	SLASH_ENCHANTRIX1 = "/enchantrix";
+	SLASH_ENCHANTRIX2 = "/enchant";
+	SLASH_ENCHANTRIX3 = "/enx";
+	SlashCmdList["ENCHANTRIX"] = function(msg) Enchantrix.Command.HandleCommand(msg) end
+
+	Stubby.RegisterEventHook("ADDON_LOADED", "Enchantrix", addonLoaded)
+end
+
+local ns = function(v) return v or "None" end
+function pickupInventoryItemHook(slot)
+	-- Remember last activated item
+	if (not UnitCastingInfo("player")) then
+		if slot then
+			DisenchantEvent.spellTarget = GetInventoryItemLink("player", slot)
+			DisenchantEvent.targetted = GetTime()
+		end
+	end
+end
+
+function pickupContainerItemHook(bag, slot)
+	-- Remember last activated item
+	if (not UnitCastingInfo("player")) then
+		if bag and slot then
+			DisenchantEvent.spellTarget = GetContainerItemLink(bag, slot)
+			DisenchantEvent.targetted = GetTime()
+		end
+	end
+end
+
+function onEvent(funcVars, event, player, spell, rank, target)
+	if event == "UNIT_SPELLCAST_SUCCEEDED" then
+		DisenchantEvent.finished = nil
+		if spell == _ENCH('ArgSpellname') then
+			if (DisenchantEvent.spellTarget and GetTime() - DisenchantEvent.targetted < 10) then
+				DisenchantEvent.finished = DisenchantEvent.spellTarget
+			end
+		end
+	elseif event == "UNIT_SPELLCAST_FAILED" or event == "UNIT_SPELLCAST_INTERRUPTED" then
+		DisenchantEvent.finished = nil
+	elseif event == "LOOT_OPENED" then
+		local now = GetTime()
+		if DisenchantEvent.finished then
+			Enchantrix.Util.ChatPrint(_ENCH("FrmtFound"):format(DisenchantEvent.finished))
+			local sig = Enchantrix.Util.GetSigFromLink(DisenchantEvent.finished)
+			for i = 1, GetNumLootItems(), 1 do
+				if LootSlotIsItem(i) then
+					local icon, name, quantity, rarity = GetLootSlotInfo(i)
+					local link = GetLootSlotLink(i)
+					Enchantrix.Util.ChatPrint(("  %s x%d"):format(link, quantity))
+					-- Save result
+					local reagentID = Enchantrix.Util.GetItemIdFromLink(link)
+					if reagentID then
+						Enchantrix.Storage.SaveDisenchant(sig, reagentID, quantity)
+					end
+				end
+			end
+		end
+		DisenchantEvent.spellTarget = nil
+		DisenchantEvent.targetted = nil
+		DisenchantEvent.finished = nil
+	end
+end
+
+-- Execute on load
+onLoad()
