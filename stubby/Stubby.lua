5f437e4b3edc77e7603dcde854ae5e25cda8983b
tml <tml@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2007-01-02 04:38:50 +0000

    fixed all references to the "GLP" to be "GPL" instead.
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@1285 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- stubby/Stubby.lua
+++ stubby/Stubby.lua
@@ -161,7 +161,7 @@ end
 		GNU General Public License for more details.
 
 		You should have received a copy of the GNU General Public License
-		along with this program(see GLP.txt); if not, write to the Free Software
+		along with this program(see GPL.txt); if not, write to the Free Software
 		Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 	Note:
