7c777063406ff345fc28ec5e13c9780d60a81486
normal <normal@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2007-01-05 21:12:44 +0000

    Adjust debugPrint message to match [1302]
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@1303 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- auctioneer/AucQueryManager.lua
+++ auctioneer/AucQueryManager.lua
@@ -564,7 +564,7 @@ function onAuctionItemListUpdate()
 			-- Empty string owner alone is akin to 'Unknown Entity'.
 			--   should cause 2 more attempts at scanning the page, then the item should be skipped.
 			if (auction.owner == nil or auction.name == nil) then 
-				debugPrint("onAuctionItemListUpdate() - Nil owner and name.");
+				debugPrint("onAuctionItemListUpdate() - Nil owner or name.");
 				-- todo: stuff
 				break;
 			end
