b2a50bb55e38f67c28af09fab793aee5f8802efa
norganna <norganna@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2007-01-12 10:02:34 +0000

    Change Auctioneer.RegisterRevision to Auctioneer_RegisterRevision so it can be loaded before the Auctioneer table is initialized. Make the UserInterface XML files responsible for loading their own lua files.
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@1330 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- auctioneer/AucRevision.lua
+++ auctioneer/AucRevision.lua
@@ -29,18 +29,20 @@
 		http://www.fsf.org/licensing/licenses/gpl-faq.html#InterpreterIncompat
 ]]
 
-Auctioneer.Revisions = { }
+if (Auctioneer_RegisterRevision) then return end
+
+local revs = { }
 local dist = {
 --[[<%revisions%>]]
 }
 
-function Auctioneer.RegisterRevision(path, revision)
+function Auctioneer_RegisterRevision(path, revision)
 	local _,_, file = path:find("%$URL: .*/auctioneer/([^%$]+) %$")
 	local _,_, rev = revision:find("%$Rev: (%d+) %$")
 	if not file then return end
 	if not rev then rev = 0 else rev = tonumber(rev) or 0 end
 
-	Auctioneer.Revisions[file] = rev
+	revs[file] = rev
 	if (nLog) then
 		nLog.AddMessage("Auctioneer", "AucRevision", N_INFO, "Loaded revisioned file", "Loaded", file, "revision", rev)
 	end
@@ -48,13 +50,50 @@ end
 Auctioneer.RegisterRevision("$URL$", "$Rev$")
 
 
-function Auctioneer.Validate()
-	for file, revision in dist do
-		local current = Auctioneer.Revisions[file]
+local messageFrame
+local function message(msg)
+	if not messageFrame then
+		messageFrame = CreateFrame("Frame", "", UIParent)
+		messageFrame:SetPoint("TOP", UIParent, "TOP", 0, 150)
+		messageFrame:SetWidth(300);
+		messageFrame:SetWidth(200);
+		messageFrame:SetFrameStrata("DIALOG")
+		messageFrame:SetBackdrop({
+			bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+			edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
+			tile = true, tileSize = 32, edgeSize = 16,
+			insets = { left = 4, right = 4, top = 4, bottom = 4 }
+		})
+		messageFrame:SetBackdropColor(0.5,0,0, 0.8)
+
+		messageFrame.done = CreateFrame("Button", "", messageFrame, "OptionsButtonTemplate")
+		messageFrame.done:SetText(OKAY)
+		messageFrame.done:SetPoint("BOTTOMRIGHT", messageFrame, "BOTTOMRIGHT", -10, 10)
+		messageFrame.done:SetScript("OnClick", function() messageFrame:Hide() end)
+
+		messageFrame.text = messageFrame:CreateFontString("", "HIGH")
+		messageFrame.text:SetPoint("TOPLEFT", messageFrame, "TOPLEFT")
+		messageFrame.text:SetPoint("BOTTOMRIGHT", messageFrame.done, "TOPRIGHT")
+		messageFrame.text:SetFont("Fonts\\FRIZQT__.TTF",12)
+		messageFrame.text:SetJustifyH("LEFT")
+	end
+	messageFrame.text:SetText(msg)
+	messageFrame:Show()
+end
+
+function Auctioneer_Validate()
+	local matches = true
+	for file, revision in pairs(dist) do
+		local current = revs[file]
 		if (not current or current ~= revision) then
-			message("Warning: Your Auctioneer installation appears to have mismatching file versions.\nPlease make sure you delete the old AddOns/Auctioneer directory, reinstall a fresh copy and restart WoW completely before reporting any bugs.\nThankyou.")
-			return false
+			matches = false
+			if (nLog) then
+				nLog.AddMessage("Auctioneer", "Validate", N_WARNING, "File revision mismatch", "File", file, "should be revision", revision, "but is actually", current)
+			end
 		end
+	end
+	if (not matches) then
+		message("Warning: Your Auctioneer installation appears to have mismatching file versions.\nPlease make sure you delete the old AddOns/Auctioneer directory, reinstall a fresh copy and restart WoW completely before reporting any bugs.\nThankyou.")
 	end
 	return true
 end
