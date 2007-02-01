wvangeit/Auctioneer

commit bb76c20fa8968eebd52e999357c13a73cac8e4dd

Initial commit of Lua 5.1 versions of EnhTooltip and Stubby. NOTE: These versions are not yet WoW 2.0 compatible.
git-svn-id: http://svn.norganna.org/auctioneer/trunk@1069 21a0ffd4-88d5-4261-b61c-461777ffcffb

stubby/Stubby.lua
@@ -12,14 +12,14 @@ loading.
 
 A quick example of this is:
 -------------------------------------------
-	Stubby.RegisterBootCode("myAddOn", "CommandHandler", [[
+	Stubby.RegisterBootCode("myAddOn", "CommandHandler", [=[
 		local function cmdHandler(msg)
 			LoadAddOn("myAddOn")
 			MyAddOn_Command(msg)
 		end
 		SLASH_MYADDON1 = "/myaddon"
 		SlashCmdList['MYADDON'] = cmdHandler
-	]]);
+	]=]);
 -------------------------------------------
 So, what did this just do? It registered some boot code
 (called "CommandHandler") with Stubby that Stubby will
@@ -163,7 +163,7 @@ end
 		You should have received a copy of the GNU General Public License
 		along with this program(see GLP.txt); if not, write to the Free Software
 		Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
---]]
+]]
 
 local cleanList
 local config = {
@@ -216,19 +216,19 @@ local unregisterFunctionHook		-- unregisterFunctionHook(triggerFunction, hookFun
 -- and assigns an actual ordering to them.
 function rebuildNotifications(notifyItems)
 	local notifyFuncs = {}
-	for hookType, hData in notifyItems do
+	for hookType, hData in pairs(notifyItems) do
 		notifyFuncs[hookType] = {}
 
 		-- Sort all hooks for this type in ascending numerical order.
 		local sortedPositions = {}
-		for requestedPos,_ in hData do
+		for requestedPos in ipairs(hData) do
 			table.insert(sortedPositions, requestedPos)
 		end
 		table.sort(sortedPositions)
 
 		-- Process the sorted request list and insert in correct
 		-- order into the call list.
-		for _,requestedPos in sortedPositions do
+		for _,requestedPos in ipairs(sortedPositions) do
 			local func = hData[requestedPos]
 			table.insert(notifyFuncs[hookType], func)
 		end
@@ -342,8 +342,19 @@ end
 -- in between your hook and the original.
 function registerFunctionHook(triggerFunction, position, hookFunc, ...)
 	local insertPos = tonumber(position) or 200
-	local funcObj = { f=hookFunc, a=arg, p=position }
-	if (table.getn(arg) == 0) then funcObj.a = nil; end
+	local funcObj
+	if (select("#", ...) == 0) then
+		funcObj = {
+			f = hookFunc,
+			p = position,
+		}
+	else
+		funcObj = {
+			f = hookFunc,
+			a = {select(1, ...)},
+			p = position
+		}
+	end
 
 	if (not config.calls) then config.calls = {} end
 	if (not config.calls.functions) then config.calls.functions = {} end
@@ -366,7 +377,7 @@ end
 
 function unregisterFunctionHook(triggerFunction, hookFunc)
 	if not (config.calls and config.calls.functions and config.calls.functions[triggerFunction]) then return end
-	for pos, funcObj in config.calls.functions[triggerFunction] do
+	for pos, funcObj in ipairs(config.calls.functions[triggerFunction]) do
 		if (funcObj and funcObj.f == hookFunc) then
 			config.calls.functions[triggerFunction][pos] = nil
 		end
@@ -379,19 +390,28 @@ end
 -- but not before)
 function registerAddOnHook(triggerAddOn, ownerAddOn, hookFunction, ...)
 	if (IsAddOnLoaded(triggerAddOn)) then
-		hookFunction(unpack(arg))
+		hookFunction(...)
 	else
-		local addon = string.lower(triggerAddOn)
+		local addon = triggerAddOn:lower()
 		if (not config.loads[addon]) then config.loads[addon] = {} end
 		config.loads[addon][ownerAddOn] = nil
 		if (hookFunction) then
-			config.loads[addon][ownerAddOn] = { f=hookFunction, a=arg }
+			if (select("#", ...) == 0) then
+				config.loads[addon][ownerAddOn] = {
+					f = hookFunction,
+				}
+			else
+				config.loads[addon][ownerAddOn] = {
+					f = hookFunction,
+					a = {select(1, ...)},
+				}
+			end
 		end
 	end
 end
 
 function unregisterAddOnHook(triggerAddOn, ownerAddOn)
-	local addon = string.lower(triggerAddOn)
+	local addon = triggerAddOn:lower()
 	if (config.loads and config.loads[addon] and config.loads[addon][ownerAddOn]) then
 		config.loads[addon][ownerAddOn] = nil
 	end
@@ -399,10 +419,10 @@ end
 
 
 function loadWatcher(loadedAddOn)
-	local addon = string.lower(loadedAddOn)
+	local addon = loadedAddOn:lower()
 	if (config.loads[addon]) then
 		local ownerAddOn, hookDetail
-		for ownerAddOn, hookDetail in config.loads[addon] do
+		for ownerAddOn, hookDetail in pairs(config.loads[addon]) do
 			hookDetail.f(hookDetail.a)
 		end
 	end
@@ -418,7 +438,16 @@ function registerEventHook(triggerEvent, ownerAddOn, hookFunction, ...)
 	end
 	config.events[triggerEvent][ownerAddOn] = nil
 	if (hookFunction) then
-		config.events[triggerEvent][ownerAddOn] = { f=hookFunction, a=arg }
+		if (select("#", ...) == 0) then
+			config.events[triggerEvent][ownerAddOn] = {
+				f = hookFunction,
+			}
+		else
+			config.events[triggerEvent][ownerAddOn] = {
+				f = hookFunction,
+				a = {select(1, ...)},
+			}
+		end
 	end
 end
 
@@ -431,7 +460,7 @@ end
 function eventWatcher(event)
 	if (config.events[event]) then
 		local ownerAddOn, hookDetail
-		for ownerAddOn, hookDetail in config.events[event] do
+		for ownerAddOn, hookDetail in pairs(config.events[event]) do
 			hookDetail.f(hookDetail.a, event, arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9,arg10,arg11,arg12,arg13,arg14,arg15,arg16,arg17,arg18,arg19,arg20);
 		end
 	end
@@ -444,8 +473,8 @@ end
 -- command handler, hook into functions, load your addon etc.
 -- Leaving bootCode nil will remove your boot.
 function registerBootCode(ownerAddOn, bootName, bootCode)
-	local ownerIndex = string.lower(ownerAddOn)
-	local bootIndex = string.lower(bootName)
+	local ownerIndex = ownerAddOn:lower()
+	local bootIndex = bootName:lower()
 	if (not StubbyConfig.boots) then StubbyConfig.boots = {} end
 	if (not StubbyConfig.boots[ownerIndex]) then StubbyConfig.boots[ownerIndex] = {} end
 	StubbyConfig.boots[ownerIndex][bootIndex] = nil
@@ -455,8 +484,8 @@ function registerBootCode(ownerAddOn, bootName, bootCode)
 end
 
 function unregisterBootCode(ownerAddOn, bootName)
-	local ownerIndex = string.lower(ownerAddOn)
-	local bootIndex = string.lower(bootName)
+	local ownerIndex = ownerAddOn:lower()
+	local bootIndex = bootName:lower()
 	if not (StubbyConfig.boots) then return end
 	if not (ownerIndex and StubbyConfig.boots[ownerIndex]) then return end
 	if (bootIndex == nil) then
@@ -513,7 +542,7 @@ function checkAddOns()
 			end
 		end
 	end
-	for name,_ in StubbyConfig.inspected do
+	for name in pairs(StubbyConfig.inspected) do
 		if (not goodList[name]) then
 			StubbyConfig.inspected[name] = nil
 			StubbyConfig.addinfo[name] = nil
@@ -546,7 +575,7 @@ end
 function cleanUpAddOnConfigs()
 	if (not cleanList) then return; end
 
-	local addonIndex = table.getn(cleanList)
+	local addonIndex = #cleanList
 	local addonName = cleanList[addonIndex]
 
 	if (addonIndex == 1) then
@@ -592,7 +621,7 @@ function searchForNewAddOns()
 		if (IsAddOnLoadOnDemand(i) and shouldInspectAddOn(name) and loadable) then
 			local addonDeps = { GetAddOnDependencies(i) }
 			for _, dependancy in pairs(addonDeps) do
-				if (string.lower(dependancy) == "stubby") then
+				if (dependancy:lower() == "stubby") then
 					requiresLoad = true
 				end
 			end
@@ -606,7 +635,7 @@ end
 -- related addon is not loaded yet, runs the boot script.
 function runBootCodes()
 	if (not StubbyConfig.boots) then return end
-	for addon, boots in StubbyConfig.boots do
+	for addon, boots in pairs(StubbyConfig.boots) do
 		if (not IsAddOnLoaded(addon) and IsAddOnLoadOnDemand(addon)) then
 			local _, _, _, _, loadable = GetAddOnInfo(addon)
 			if (loadable) then
@@ -634,8 +663,15 @@ function onWorldStart()
 end
 
 function onLoaded()
-	if not StubbyConfig.inspected then StubbyConfig.inspected = {} end
-	if not StubbyConfig.addinfo then StubbyConfig.addinfo = {} end
+	if (not (type(StubbyConfig) == "table")) then
+		StubbyConfig = {};
+	end
+	if (not StubbyConfig.inspected) then
+		StubbyConfig.inspected = {};
+	end
+	if (not StubbyConfig.addinfo) then
+		StubbyConfig.addinfo = {};
+	end
 	Stubby.RegisterEventHook("PLAYER_LOGIN", "Stubby", onWorldStart)
 end
 
@@ -643,7 +679,7 @@ function events(event, param)
 	if (not event) then event = "" end
 	if (not param) then param = "" end
 	if (event == "ADDON_LOADED") then
-		if (string.lower(param) == "stubby") then onLoaded() end
+		if (param:lower() == "stubby") then onLoaded() end
 		Stubby.LoadWatcher(param)
 	end
 	Stubby.EventWatcher(event)
@@ -652,9 +688,11 @@ end
 function chatPrint(...)
 	if ( DEFAULT_CHAT_FRAME ) then
 		local msg = ""
-		for i=1, table.getn(arg) do
-			if i==1 then msg = arg[i]
-			else msg = msg.." "..arg[i]
+		for i = 1, select("#", ...) do
+			if (i == 1) then
+				msg = select(i, ...)
+			else
+				msg = msg.." "..select(i, ...)
 			end
 		end
 		DEFAULT_CHAT_FRAME:AddMessage(msg, 1.0, 0.35, 0.15)
@@ -664,10 +702,10 @@ end
 -- This function allows boot code to store a configuration variable
 -- by default the variable is per character unless isGlobal is set.
 function setConfig(ownerAddOn, variable, value, isGlobal)
-	local ownerIndex = string.lower(ownerAddOn)
-	local varIndex = string.lower(variable)
+	local ownerIndex = ownerAddOn:lower()
+	local varIndex = variable:lower()
 	if (not isGlobal) then
-		varIndex = string.lower(UnitName("player")) .. ":" .. varIndex
+		varIndex = UnitName("player"):lower() .. ":" .. varIndex
 	end
 
 	if (not StubbyConfig.configs) then StubbyConfig.configs = {} end
@@ -679,9 +717,9 @@ end
 -- it will prefer a player specific variable over a global with the
 -- same name
 function getConfig(ownerAddOn, variable)
-	local ownerIndex = string.lower(ownerAddOn)
-	local globalIndex = string.lower(variable)
-	local playerIndex = string.lower(UnitName("player")) .. ":" .. globalIndex
+	local ownerIndex = ownerAddOn:lower()
+	local globalIndex = variable:lower()
+	local playerIndex = UnitName("player"):lower() .. ":" .. globalIndex
 
 	if (not StubbyConfig.configs) then return end
 	if (not StubbyConfig.configs[ownerIndex]) then return end
@@ -696,12 +734,12 @@ end
 -- global and player specific) or all config variables for the
 -- ownerAddOn if no variable is specified
 function clearConfig(ownerAddOn, variable)
-	local ownerIndex = string.lower(ownerAddOn)
+	local ownerIndex = ownerAddOn:lower()
 	if (not StubbyConfig.configs) then return end
 	if (not StubbyConfig.configs[ownerIndex]) then return end
 	if (variable) then
-		local globalIndex = string.lower(variable)
-		local playerIndex = string.lower(UnitName("player")) .. ":" .. globalIndex
+		local globalIndex = variable:lower()
+		local playerIndex = UnitName("player"):lower() .. ":" .. globalIndex
 		StubbyConfig.configs[ownerIndex][globalIndex] = nil
 		StubbyConfig.configs[ownerIndex][playerIndex] = nil
 	else
@@ -711,9 +749,7 @@ end
 
 -- Extract the revision number from SVN keyword string
 local function getRevision()
-	local found, _, rev = string.find("$Revision$", "(%d+)")
-	if (found ~= nil) then return tonumber(rev); end
-	return nil
+	return tonumber(("$Revision$"):match("(%d+)"))
 end
 
 -- Setup our Stubby global object. All interaction is done

enhtooltip/EnhTooltip.toc
@@ -9,7 +9,5 @@
 ## Revision: $Id$
 ## URL: http://auctioneeraddon.com/dl/EnhTooltip
 
-TinyMoneyFrame.lua
 TinyMoneyFrame.xml
 Tooltip.lua
-Tooltip.xml

enhtooltip/TinyMoneyFrame.xml
@@ -4,7 +4,7 @@ xsi:schemaLocation="http://www.blizzard.com/wow/ui/ ..\FrameXML\UI.xsd">
 <!--
 	Version: <%version%> (<%codename%>)
 	Revision: $Id$
-
+	
 	License:
 		This program is free software; you can redistribute it and/or
 		modify it under the terms of the GNU General Public License

enhtooltip/Tooltip.lua
@@ -314,7 +314,7 @@ function getglobalIterator(fmt, first, last)
 		if last and (i > last) then
 			return
 		end
-		local obj = getglobal(string.format(fmt, i))
+		local obj = getglobal(fmt:format(i))
 		i = i + 1
 		return obj
 	end
@@ -343,10 +343,9 @@ function clearTooltip()
 	EnhancedTooltip.lineCount = 0
 	EnhancedTooltip.moneyCount = 0
 	EnhancedTooltip.minWidth = 0
-	for curLine in self.embedLines do
+	for curLine in pairs(self.embedLines) do
 		self.embedLines[curLine] = nil;
 	end
-	table.setn(self.embedLines, 0);
 end
 
 function getRect(object, curRect)
@@ -544,15 +543,15 @@ function getTextGSC(money, exact, dontUseColorCodes)
 	if (not dontUseColorCodes) then
 		local fmt = GSC_START
 		if (g > 0) then
-			gsc = gsc..string.format(fmt, GSC_GOLD, g)
+			gsc = gsc..fmt:format(GSC_GOLD, g)
 			fmt = GSC_PART
 		end
 		if (s > 0) or (c > 0) then
-			gsc = gsc..string.format(fmt, GSC_SILVER, s)
+			gsc = gsc..fmt:format(GSC_SILVER, s)
 			fmt = GSC_PART
 		end
 		if (c > 0) then
-			gsc = gsc..string.format(fmt, GSC_COPPER, c)
+			gsc = gsc..fmt:format(GSC_COPPER, c)
 		end
 		if (gsc == "") then
 			gsc = GSC_NONE
@@ -575,7 +574,7 @@ function getTextGSC(money, exact, dontUseColorCodes)
 end
 
 function embedRender()
-	for pos, lData in self.embedLines do
+	for pos, lData in pairs(self.embedLines) do
 		self.currentGametip:AddLine(lData.line)
 		if (lData.r) then
 			local lastLine = getglobal(self.currentGametip:GetName().."TextLeft"..self.currentGametip:NumLines())
@@ -654,7 +653,7 @@ end
 
 function lineColor(r, g, b)
 	if (EnhancedTooltip.curEmbed) and (self.currentGametip) then
-		local n = table.getn(self.embedLines)
+		local n = #self.embedLines
 		self.embedLines[n].r = r
 		self.embedLines[n].g = g
 		self.embedLines[n].b = b
@@ -749,8 +748,7 @@ function linkType(link)
 	if type(link) ~= "string" then
 		return
 	end
-	local _, _, linktype = string.find(link, "|H(%a+):")
-	return linktype
+	return link:match("|H(%a+):")
 end
 
 function nameFromLink(link)
@@ -758,37 +756,27 @@ function nameFromLink(link)
 	if( not link ) then
 		return
 	end
-	_, _, name = string.find(link, "|c%x+|Hitem:%d+:%d+:%d+:%d+|h%[(.-)%]|h|r");
-	if (name) then
-		return name;
-	end
-	return
+	return link:match("|c%x+|Hitem:%d+:%d+:%d+:%d+|h%[(.-)%]|h|r");
 end
 
 function hyperlinkFromLink(link)
 	if( not link ) then
 		return
 	end
-	_, _, hyperlink = string.find(link, "|H([^|]+)|h");
-	if (hyperlink) then
-		return hyperlink;
-	end
+	return link:match("|H([^|]+)|h");
 end
 
 function baselinkFromLink(link)
 	if( not link ) then
 		return
 	end
-	_, _, baselink = string.find(link, "|Hitem:(%d+:%d+:%d+):%d+|h");
-	if (baselink) then
-		return baselink;
-	end
+	return link:match("|Hitem:(%d+:%d+:%d+):%d+|h");
 end
 
 function qualityFromLink(link)
 	if (not link) then return end
-	local _, _, color = string.find(link, "(|c%x+)|Hitem:%d+:%d+:%d+:%d+|h%[.-%]|h|r");
-	if color then
+	local color link:match("(|c%x+)|Hitem:%d+:%d+:%d+:%d+|h%[.-%]|h|r");
+	if (color) then
 		for i = 0, 6 do
 			local _, _, _, hex = GetItemQualityColor(i)
 			if color == hex then
@@ -1034,6 +1022,7 @@ function gtHookSetInboxItem(funcArgs, retVal, frame, index)
 			break
 		end
 	end
+
 end
 
 function gtHookSetInventoryItem(funcArgs, retVal, frame, unit, slot)
@@ -1116,7 +1105,7 @@ end
 -- Given a Blizzard item link, breaks it into it's itemID, randomProperty, enchantProperty, uniqueness and name
 function breakLink(link)
 	if (type(link) ~= 'string') then return end
-	local i,j, itemID, enchant, randomProp, uniqID, name = string.find(link, "|Hitem:(%d+):(%d+):(%d+):(%d+)|h[[]([^]]+)[]]|h")
+	local itemID, enchant, randomProp, uniqID, name = link:match("|Hitem:(%d+):(%d+):(%d+):(%d+)|h[[]([^]]+)[]]|h")
 	return tonumber(itemID or 0), tonumber(randomProp or 0), tonumber(enchant or 0), tonumber(uniqID or 0), name
 end
 
@@ -1209,7 +1198,7 @@ end
 function getLootLinkLink(name)
 	local itemLink = ItemLinks[name]
 	if (itemLink and itemLink.c and itemLink.i and LootLink_CheckItemServer(itemLink, getLootLinkServer())) then
-		local item = string.gsub(itemLink.i, "(%d+):(%d+):(%d+):(%d+)", "%1:0:%3:%4")
+		local item = itemLink.i:gsub("(%d+):(%d+):(%d+):(%d+)", "%1:0:%3:%4")
 		local link = "|c"..itemLink.c.."|Hitem:"..item.."|h["..name.."]|h|r"
 		return link
 	end
@@ -1259,8 +1248,9 @@ end
 
 local function dump(...)
 	local out = "";
-	for i = 1, arg.n, 1 do
-		local d = arg[i];
+	local numVarArgs = select("#", ...);
+	for i = 1, numVarArgs do
+		local d = select(i, ...);
 		local t = type(d);
 		if (t == "table") then
 			out = out .. "{";
@@ -1288,33 +1278,37 @@ local function dump(...)
 				out = out .. "false";
 			end
 		else
-			out = out .. string.upper(t) .. "??";
+			out = out .. t:upper() .. "??";
 		end
 
-		if (i < arg.n) then out = out .. ", "; end
+		if (i < numVarArgs) then out = out .. ", "; end
 	end
 	return out;
 end
 
 function debugPrint(...)
-	local debugWin = 0;
-	local name, shown;
+	local debugWin;
 	for i=1, NUM_CHAT_WINDOWS do
-		name,_,_,_,_,_,shown = GetChatWindowInfo(i);
-		if (string.lower(name) == "ettdebug") then debugWin = i; break; end
+		if (GetChatWindowInfo(i):lower() == "ettdebug") then
+			debugWin = i;
+			break;
+		end
 	end
-	if (debugWin == 0) then return end
+	if (not debugWin) then
+		return
+	end
 
 	local out = "";
-	for i = 1, arg.n, 1 do
+	for i = 1, select("#", ...) do
 		if (i > 1) then out = out .. ", "; end
-		local t = type(arg[i]);
-		if (t == "string") then
-			out = out .. '"'..arg[i]..'"';
-		elseif (t == "number") then
-			out = out .. arg[i];
+		local currentArg, argType = select(i, ...)
+		argType = type(currentArg);
+		if (argType == "string") then
+			out = out .. '"'..currentArg..'"';
+		elseif (argType == "number") then
+			out = out .. currentArg;
 		else
-			out = out .. dump(arg[i]);
+			out = out .. dump(currentArg);
 		end
 	end
 	getglobal("ChatFrame"..debugWin):AddMessage(out, 1.0, 1.0, 0.3);
@@ -1459,6 +1453,6 @@ EnhTooltip = {
 
 	SetElapsed			= setElapsed,
 	DebugPrint			= debugPrint,
-	
+
 	Version				= ENHTOOLTIP_VERSION,
 }

stubby/Stubby.toc
@@ -1,7 +1,7 @@
 ## Title: Stubby
 ## Notes: Allows on-demand addons to load automatically based upon simple event notification hooks [<%version%>] This AddOn is licenced under the GNU GPL, see GPL.txt for details.
 ##
-## Interface: 11200
+## Interface: 20000
 ## SavedVariables: StubbyConfig
 ##
 ## Version: <%version%> (<%codename%>)

commit 0b0698062eaffa7babaa24e3d988c1937edb0a81

* fixed a bug introduced in r1069 which rendered the unregisterFunctionHook to not work correctly
config.calls.functions[triggerFunction] contains a table of [pos]=[function] where pos might be any number. Therefore iterating over this table has to use pairs, instead of ipairs, as used before.
I'm not absolutely sure what bugs this might fix, but crossreferences to this function can be found in Auctioneer and Beancounter. Since I don't know of any tickets which might refere to this bug, I didn't investigate it further.
All other ipairs()-usages in Stubby seem to be correct.

git-svn-id: http://svn.norganna.org/auctioneer/trunk@1415 21a0ffd4-88d5-4261-b61c-461777ffcffb

stubby/Stubby.lua
@@ -432,7 +432,7 @@ end
 
 function unregisterFunctionHook(triggerFunction, hookFunc)
 	if not (config.calls and config.calls.functions and config.calls.functions[triggerFunction]) then return end
-	for pos, funcObj in ipairs(config.calls.functions[triggerFunction]) do
+	for pos, funcObj in pairs(config.calls.functions[triggerFunction]) do
 		if (funcObj and funcObj.f == hookFunc) then
 			config.calls.functions[triggerFunction][pos] = nil
 		end