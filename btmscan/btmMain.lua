wvangeit/Auctioneer

commit 2f9080a113a92ff58d3ffb98143ce429342adb4f

code optimisations
Changed the name of the CanSendAuctionQuery()-hook to PostCanSendAuctionQuery() in order to match the same function name, used in Auctioneer.AucQueryManager, which should make the code easier to understand.

git-svn-id: http://svn.norganna.org/auctioneer/trunk@1519 21a0ffd4-88d5-4261-b61c-461777ffcffb

btmscan/btmMain.lua
@@ -79,7 +79,7 @@ BtmScan.OnLoad = function ()
 
 	Stubby.RegisterFunctionHook("EnhTooltip.AddTooltip", 600, BtmScan.TooltipHook)
 	Stubby.RegisterFunctionHook("QueryAuctionItems", 600, BtmScan.QueryAuctionItems)
-	Stubby.RegisterFunctionHook("CanSendAuctionQuery", 10, BtmScan.CanSendAuctionQuery)
+	Stubby.RegisterFunctionHook("CanSendAuctionQuery", 10, BtmScan.PosCanSendAuctionQuery)
 
 	-- Register our temporary command hook with stubby
 	Stubby.RegisterBootCode("BtmScan", "CommandHandler", [[
@@ -728,7 +728,7 @@ BtmScan.isDEAble = function(itemMinLevel)
 	end
 end
 
-BtmScan.CanSendAuctionQuery = function(_, _, noHook, who)
+BtmScan.PostCanSendAuctionQuery = function(_, _, noHook, who)
 	-- We don't care about nohook - we need the scan to stop!
 	if (BtmScan.scanStage and BtmScan.scanStage > 0 and (not who or who ~= "btmscan")) then
 		return "setreturn", {}

commit 807c356b331f064de78c80f5ae609c159adcb060

Fixed a typo introduced in r1519 - thx to MentalPower
git-svn-id: http://svn.norganna.org/auctioneer/trunk@1522 21a0ffd4-88d5-4261-b61c-461777ffcffb

btmscan/btmMain.lua
@@ -85,7 +85,7 @@ BtmScan.OnLoad = function ()
 
 	Stubby.RegisterFunctionHook("EnhTooltip.AddTooltip", 600, BtmScan.TooltipHook)
 	Stubby.RegisterFunctionHook("QueryAuctionItems", 600, BtmScan.QueryAuctionItems)
-	Stubby.RegisterFunctionHook("CanSendAuctionQuery", 10, BtmScan.PosCanSendAuctionQuery)
+	Stubby.RegisterFunctionHook("CanSendAuctionQuery", 10, BtmScan.PostCanSendAuctionQuery)
 
 	-- Register our temporary command hook with stubby
 	Stubby.RegisterBootCode("BtmScan", "CommandHandler", [[