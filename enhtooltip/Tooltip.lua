wvangeit/Auctioneer

commit 1dab15a95a278915a4fbc80a7547b8a7ca762155

(fixes #1519) EnhTT will no longer move the tooltip frame, if it's not absolutely necessary.
Reverted changes made to tooltip.lua starting r1606, since the correct behaviour should be to check for currentTooltipOwner == UIParent. I double checked the current code against the bagnon-usage, and can't reproduce the bug reported in #1512). So either bagnon had a bug, which has been fixed meanwhile, the old check (:GetName() == "UIParent") does not equal the new one (Tooltip == UIParent) or the bug was not caused by that part of the code at all.

git-svn-id: http://svn.norganna.org/auctioneer/trunk@1765 21a0ffd4-88d5-4261-b61c-461777ffcffb

enhtooltip/Tooltip.lua
@@ -584,12 +584,11 @@ function anchorEnhancedTooltip(currentTooltip, requestedHeight, requestedWidth)
 		   -- display EnhTooltip below the currentTooltip
 			EnhancedTooltip:SetPoint("TOPLEFT", currentTooltip, "BOTTOMLEFT", 0, 0)
 		end
-	elseif not currentTooltipOwner then
-		-- TODO: acording to wowwiki.com this should never happen => test it and probably add a debugMessage for user testing
-		-- If currentTooltipOwner is nil, the current tooltip is not attached to
-		-- any other frame, so we don't have to bother about correct alignment.
-		-- The only thing todo is put the object underneath / shuffle it up, if
-		-- if there aint enuff room.
+	elseif currentTooltipOwner == UIParent then
+		-- If currentTooltipOwner is UIParent, the current tooltip is not
+		-- attached to any other frame, so we don't have to bother about correct
+		-- alignment. The only thing to do is put the object
+		-- underneath / shuffle it up, if there ain't enuough room.
 		private.showIgnore = true
 		currentTooltip:Show()
 		private.showIgnore = false

commit 894f3dda02be513226221616f3e900d9191ee180

void
fixes a bug introduced in r1765: According to wowwiki, setOwner supports nil as the frame's owner which is the same as using UIParent. Though it does not look like Blizzard uses it in its own functions (instead they use UIParent), it can (and is) be(ing) used by 3rd party developers. This has been taken care of now. Therefore #1512 is now properly fixed and should work correctly (including proper alignment - see #1519).

git-svn-id: http://svn.norganna.org/auctioneer/trunk@1771 21a0ffd4-88d5-4261-b61c-461777ffcffb

enhtooltip/Tooltip.lua
@@ -584,8 +584,8 @@ function anchorEnhancedTooltip(currentTooltip, requestedHeight, requestedWidth)
 		   -- display EnhTooltip below the currentTooltip
 			EnhancedTooltip:SetPoint("TOPLEFT", currentTooltip, "BOTTOMLEFT", 0, 0)
 		end
-	elseif currentTooltipOwner == UIParent then
-		-- If currentTooltipOwner is UIParent, the current tooltip is not
+	elseif not currentTooltipOwner or (currentTooltipOwner == UIParent) then
+		-- If currentTooltipOwner is nil or UIParent, the current tooltip is not
 		-- attached to any other frame, so we don't have to bother about correct
 		-- alignment. The only thing to do is put the object
 		-- underneath / shuffle it up, if there ain't enuough room.