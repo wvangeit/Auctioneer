8f9f5a0cb8b66eaa77aa2d553e563fbd0b4b3770
norganna <norganna@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2007-09-03 02:23:04 +0000

    Update LibStub to current trunk revision from http://svn.wowace.com/wowace/trunk/LibStub/LibStub.lua
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@2113 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- !nconfigator/nPanelScroller.lua
+++ !nconfigator/nPanelScroller.lua
@@ -27,18 +27,18 @@ local LIBRARY_VERSION_MAJOR = "nPanelScroller"
 local LIBRARY_VERSION_MINOR = 1
 
 do -- LibStub
-	-- Id: LibStub.lua 47555 2007-08-27 18:33:13Z kaelten
+	-- LibStub is a simple versioning stub meant for use in Libraries.  http://www.wowace.com/wiki/LibStub for more info
 	-- LibStub is hereby placed in the Public Domain
 	-- Credits: Kaelten, Cladhaire, ckknight, Mikk, Ammo, Nevcairiel, joshborke
-	local LIBSTUB_MAJOR, LIBSTUB_MINOR = "LibStub", 0
+	local LIBSTUB_MAJOR, LIBSTUB_MINOR = "LibStub", 2  -- NEVER MAKE THIS AN SVN REVISION! IT NEEDS TO BE USABLE IN ALL REPOS!
 	local LibStub = _G[LIBSTUB_MAJOR]
 
 	-- Check to see is this version of the stub is obsolete
-	if not LibStub or not LibStub.minors[LIBSTUB_MAJOR] or LibStub.minors[LIBSTUB_MAJOR] < LIBSTUB_MINOR then
+	if not LibStub or LibStub.minor < LIBSTUB_MINOR then
 		LibStub = LibStub or {libs = {}, minors = {} }
 		_G[LIBSTUB_MAJOR] = LibStub
-		LibStub.libs[LIBSTUB_MAJOR], LibStub.minors[LIBSTUB_MAJOR] = LibStub, LIBSTUB_MINOR
-
+		LibStub.minor = LIBSTUB_MINOR
+		
 		-- LibStub:NewLibrary(major, minor)
 		-- major (string) - the major version of the library
 		-- minor (string or number ) - the minor version of the library
@@ -48,13 +48,13 @@ do -- LibStub
 		function LibStub:NewLibrary(major, minor)
 			assert(type(major) == "string", "Bad argument #2 to `NewLibrary' (string expected)")
 			minor = assert(tonumber(strmatch(minor, "%d+")), "Minor version must either be a number or contain a number.")
-
+			
 			local oldminor = self.minors[major]
 			if oldminor and oldminor >= minor then return nil end
 			self.minors[major], self.libs[major] = minor, self.libs[major] or {}
 			return self.libs[major], oldminor
 		end
-
+		
 		-- LibStub:GetLibrary(major, [silent])
 		-- major (string) - the major version of the library
 		-- silent (boolean) - if true, library is optional, silently return nil if its not found
@@ -62,19 +62,19 @@ do -- LibStub
 		-- throws an error if the library can not be found (except silent is set)
 		-- returns the library object if found
 		function LibStub:GetLibrary(major, silent)
-			if not silent and not self.libs[major] then
+			if not self.libs[major] and not silent then
 				error(("Cannot find a library instance of %q."):format(tostring(major)), 2)
 			end
 			return self.libs[major], self.minors[major]
 		end
-
+		
 		-- LibStub:IterateLibraries()
 		-- 
 		-- Returns an iterator for the currently registered libraries
 		function LibStub:IterateLibraries() 
 			return pairs(self.libs) 
 		end
-
+		
 		setmetatable(LibStub, { __call = LibStub.GetLibrary })
 	end
 end -- LibStub
