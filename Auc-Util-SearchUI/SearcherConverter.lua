6a3b971da8fb4fa7dbbb44ce85fd305ef5443d4f
testleK <testleK@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2008-05-04 16:26:35 +0000

    (ASER-28) svn props
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@3090 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- Auc-Util-SearchUI/SearcherConverter.lua
+++ Auc-Util-SearchUI/SearcherConverter.lua
@@ -1,4 +1,33 @@
+--[[
+	Auctioneer Advanced - Search UI - Searcher Converter
+	Version: <%version%> (<%codename%>)
+	Revision: $Id$
+	URL: http://auctioneeraddon.com/
 
+	This is a plugin module for the SearchUI that assists in searching by refined paramaters
+
+	License:
+		This program is free software; you can redistribute it and/or
+		modify it under the terms of the GNU General Public License
+		as published by the Free Software Foundation; either version 2
+		of the License, or (at your option) any later version.
+
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with this program(see GPL.txt); if not, write to the Free Software
+		Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+	Note:
+		This AddOn's source code is specifically designed to work with
+		World of Warcraft's interpreted AddOn system.
+		You have an implicit licence to use this AddOn with these facilities
+		since that is its designated purpose as per:
+		http://www.fsf.org/licensing/licenses/gpl-faq.html#InterpreterIncompat
+--]]
 -- Create a new instance of our lib with our parent
 local lib, parent, private = AucSearchUI.NewSearcher("Converter")
 if not lib then return end
@@ -408,4 +437,4 @@ function lib.Search(item)
 	end
 end
 
-AucAdvanced.RegisterRevision("$URL: http://dev.norganna.org/auctioneer/trunk/Auc-Util-SearchUI/SearcherConverter.lua $", "$Rev: 2531 $")
+AucAdvanced.RegisterRevision("$URL$", "$Rev$")
