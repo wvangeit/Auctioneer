f14caabd226648720a12f01431d022b346b5e4f6
aesir <aesir@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2008-09-13 17:44:37 +0000

    Fixes BCNT-200 Truncated help text string for Available Date Commands and some miscellaneous typos.
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@3484 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- BeanCounter/BeanCounterStrings.lua
+++ BeanCounter/BeanCounterStrings.lua
@@ -25,7 +25,7 @@
 	Note:
 		This AddOn's source code is specifically designed to work with
 		World of Warcraft's interpreted AddOn system.
-		You have an implicit licence to use this AddOn with these facilities
+		You have an implicit license to use this AddOn with these facilities
 		since that is its designated purpose as per:
 		http://www.fsf.org/licensing/licenses/gpl-faq.html#InterpreterIncompat
 ]]
@@ -199,6 +199,13 @@ BeanCounterLocalizations = {
 
 	};
 
+	elGR = {
+
+		-- Section: Config Text
+		["C_MailRecolor"]	= "Mail Re-Color Method";
+
+	};
+
 	enUS = {
 
 		-- Section: Config Text
@@ -220,16 +227,16 @@ BeanCounterLocalizations = {
 
 		-- Section: Help Text
 		["A_DateString"]	= "This controls how the Date field of BeanCounter's GUI is shown. Commands are prefaced by % and multiple commands and text can be mixed. For example %a == %X would display Wed == 21:34:21";
-		["A_DateStringCommands"]	= "Commands: \n %a = abr. weekday name, \n %A = weekday name, \n %b = abr. month name, \n %B = month name,\n %c = date and time, \n %d = day of the month (01-31),\n %H = hour (24), \n %I = hour (12),\n %M = minute, \n %m = month,\n %p = a";
+		["A_DateStringCommands"]	= "Commands: \n %a = abr. weekday name, \n %A = weekday name, \n %b = abr. month name, \n %B = month name,\n %c = date and time, \n %d = day of the month (01-31),\n %H = hour (24), \n %I = hour (12),\n %M = minute, \n %m = month,\n %p = am/pm, \n %S = second,\n %U = week number of the year ,\n %w = numerical weekday (0-6),\n %x = date, \n %X = time,\n %Y = full year (2007), \n %y = two-digit year (07)";
 		["A_ExtenalSearch"]	= "Other addons can have BeanCounter search for an item to be displayed in BeanCounter's GUI. For example this allows BeanCounter to show what items you are looking at in Appraiser";
-		["A_MailInvoiceTimeout"]	= "The length of time BeanCounter will wait on the server to respond to an invoice request. A invoice is the who, what, how of an Auction house mail.";
-		["A_MailRecolor"]	= "BeanCounter reads all mail from the Auction House, This option tells BeanCounter how the user wants to Recolor the messages to make them look unread.";
+		["A_MailInvoiceTimeout"]	= "The length of time BeanCounter will wait on the server to respond to an invoice request. An invoice is the \"who\", \"what\", and \"how\" of an Auction House mail.";
+		["A_MailRecolor"]	= "BeanCounter reads all mail from the Auction House. This option tells BeanCounter how the user wants to re-color the messages to make them look unread.";
 		["HelpGuiItemBox"]	= "Drop item into box to search.";
 		["Q_DateString"]	= "Date Format to use?";
 		["Q_DateStringCommands"]	= "Acceptable Date Commands?";
 		["Q_ExtenalSearch"]	= "Allow External Addons to use BeanCounter?";
 		["Q_MailInvoiceTimeout"]	= "What is Mail Invoice Timeout?";
-		["Q_MailRecolor"]	= "What is recolor?";
+		["Q_MailRecolor"]	= "What is Mail Re-color Method?";
 
 		-- Section: Mail
 		["MailAllianceAuctionHouse"]	= "Alliance Auction House";
@@ -238,15 +245,16 @@ BeanCounterLocalizations = {
 		["MailAuctionSuccessfulSubject"]	= "Auction successful";
 		["MailAuctionWonSubject"]	= "Auction won";
 		["MailHordeAuctionHouse"]	= "Horde Auction House";
+		["MailNeutralAuctionHouse"]	= "Blackwater Auction House";
 		["MailOutbidOnSubject"]	= "Outbid on";
 		["MailSalePendingOnSubject"]	= "Sale Pending";
 
 		-- Section: Tooltip Messages
-		["TTDateString"]	= "Enter the format that you would like your date field to show. Default is %c";
-		["TTDateStringExample"]	= "Displays an example of what your formated date will look like";
+		["TTDateString"]	= "Enter the format that you would like your date field to show. Default is %c.";
+		["TTDateStringExample"]	= "Displays an example of what your formatted date will look like.";
 		["TTExtenalSearch"]	= "When entering a search in another addon, BeanCounter will also display a search for that item.";
-		["TTMailInvoiceTimeout"]	= "Chooses how long BeanCounter will attempt to get a mail invoice from the server before giving up. Lower == quicker but more chance of missing data, Higher == slower but improves chances of getting data if the Mail server is extremely busy.";
-		["TTMailRecolor"]	= "Choose how Mail will appear after BeanCounter has scanned the Mail Box.";
+		["TTMailInvoiceTimeout"]	= "Chooses how long BeanCounter will attempt to get a mail invoice from the server before giving up. Lower == quicker but more chance of missing data. Higher == slower, but improves chances of getting data if the Mail server is extremely busy.";
+		["TTMailRecolor"]	= "Choose how mail will appear after BeanCounter has scanned the Mail Box.";
 
 		-- Section: User Interface
 		["UiAddonTitle"]	= "BeanCounter: Auction History Database";
@@ -276,7 +284,6 @@ BeanCounterLocalizations = {
 		["UiPricePerHeader"]	= "Price Header";
 		["UiPurchases"]	= "Purchases";
 		["UiQuantityHeader"]	= "Qty";
-		["UiReason"]	= "Reason";
 		["UiSales"]	= "Sales";
 		["UiSearch"]	= "Search";
 		["UiSearchForLabel"]	= "Search for:";
@@ -334,9 +341,19 @@ BeanCounterLocalizations = {
 		-- Section: Config Text
 		["C_BeanCounterConfig"]	= "BeanCounter Configuration";
 		["C_BeanCounterOptions"]	= "BeanCounter Options";
-		["C_DateString"]	= "|CCFFFCC00 Format date a utiliser:";
+		["C_DateString"]	= "|CCFFFCC00 Format date à utiliser:";
 		["C_DateStringExample"]	= "|CCFFFCC00 Exemple Date:";
-		["C_ExtenalSearch"]	= "Autorise les Addons Externes a utiliser la recherche BeanCounter?";
+		["C_ExtenalSearch"]	= "Autorise les Addons Externes à utiliser la recherche BeanCounter?";
+		["C_MailInvoiceTimeout"]	= "Mail de dépassement du temps des encheres = %d secondes";
+		["C_MailRecolor"]	= "Méthode de changement de couleur de mail";
+
+		-- Section: Generic Strings
+		["NoRe-Color"]	= "Pas de changement de couleur";
+		["off"]	= "Eteint";
+		["on"]	= "Allumé";
+		["Re-ColorIcons"]	= "Changement couleur d'icones";
+		["Re-ColorIconsandText"]	= "Changement couleur d'icones et du texte";
+		["Re-ColorText"]	= "Changement couleur du texte";
 
 		-- Section: Mail
 		["MailAllianceAuctionHouse"]	= "Hôtel des ventes de l'Alliance";
@@ -346,21 +363,39 @@ BeanCounterLocalizations = {
 		["MailAuctionWonSubject"]	= "Vente gagnée";
 		["MailHordeAuctionHouse"]	= "Hôtel des ventes de la Horde";
 		["MailOutbidOnSubject"]	= "Augmenter l'offre pour";
+		["MailSalePendingOnSubject"]	= "vente en suspend";
+
+		-- Section: Tooltip Messages
+		["TTDateString"]	= "Entrez le format voulu d'affichage de votre date.Par défaut c'est %c";
+		["TTDateStringExample"]	= "Montre un exemple de votre format de date";
+		["TTExtenalSearch"]	= "Quand vous entrez une recherche dans un autre addon,BeanCounter affichera aussi une recherche pour cette objet";
+		["TTMailRecolor"]	= "Choisissez comment un mail va apparaitre apres que BeanCounter\naura scanné la boite au lettre";
 
 		-- Section: User Interface
+		["UiAddonTitle"]	= "BeanCounter: Historique de vente";
+		["UiAucExpired"]	= "Vente terminée";
+		["UiAucSuccessful"]	= "Vente réussie";
 		["UiAuctions"]	= "Enchères";
 		["UiAuctionTransaction"]	= "Enchère";
 		["UiBids"]	= "Offres";
 		["UiBidTransaction"]	= "Offre";
 		["UiBuyerSellerHeader"]	= "Acheteur/Vendeur";
 		["UiBuyTransaction"]	= "Acheter";
+		["UiClassicCheckBox"]	= "Afficher les données BeanCounter classique";
+		["UiData"]	= "Donnée";
 		["UiDateHeader"]	= "Date";
 		["UiDepositTransaction"]	= "Dépôt";
+		["UiDone"]	= "Effectué";
 		["UiExactNameSearch"]	= "Nom exact";
+		["UiFailedAuctions"]	= "Vente ratée";
+		["UiFee"]	= "Commission";
 		["UiNameHeader"]	= "Objet";
 		["UiNetHeader"]	= "Net";
 		["UiNetPerHeader"]	= "Net unit.";
+		["UiOutbid"]	= "Surenchere";
+		["UiOutbids"]	= "Surencheres";
 		["UiPriceHeader"]	= "Prix";
+		["UiPriceper"]	= "Prix/par";
 		["UiPricePerHeader"]	= "Prix unit.";
 		["UiPurchases"]	= "Achats";
 		["UiQuantityHeader"]	= "Qte";
@@ -368,9 +403,13 @@ BeanCounterLocalizations = {
 		["UiSearch"]	= "Recherche";
 		["UiSearchForLabel"]	= "Rechercher :";
 		["UiSellTransaction"]	= "Vendre";
+		["UiServer"]	= "Serveur";
 		["UiTransactions"]	= "Transactions";
 		["UiTransactionsLabel"]	= "Transactions:";
 		["UiTransactionTypeHeader"]	= "Type";
+		["UiWealth"]	= "Fortune";
+		["UiWononBid"]	= "Gagné sur enchere";
+		["UiWononBuyout"]	= "Gagné sur achat direct";
 
 	};
 
@@ -581,23 +620,79 @@ BeanCounterLocalizations = {
 
 	ruRU = {
 
+		-- Section: Config Text
+		["C_BeanCounterConfig"]	= "Параметры BeanCounter";
+		["C_BeanCounterOptions"]	= "Опции BeanCounter";
+		["C_DateString"]	= "|CCFFFCC00 Используемый формат даты:";
+		["C_DateStringExample"]	= "|CCFFFCC00 Пример даты:";
+		["C_ExtenalSearch"]	= "Позволить сторонним аддонам использовать поиск BeanCounter'а?";
+		["C_MailInvoiceTimeout"]	= "Перерыв Почтовой Накладной";
+		["C_MailRecolor"]	= "способ определения цвета почты";
+
+		-- Section: Generic Strings
+		["NoRe-Color"]	= "Не изменять цвет";
+		["off"]	= "выкл";
+		["on"]	= "вкл";
+		["Re-ColorIcons"]	= "Изменить цвет иконок";
+		["Re-ColorIconsandText"]	= "Изменить цвет иконок и текста";
+		["Re-ColorText"]	= "Изменить цвет текста";
+
+		-- Section: Help Text
+		["A_DateString"]	= "Эта опция определяет, как поле Дата BeanCounter отображает в интерфейсе. Команды перед % и несколько команд с текстом могут быть употреблятся одновременно. Для примера %a == %X покажут Пн == 21:34:21";
+		["A_DateStringCommands"]	= "Комманды \n %a = аббр. дня недели,  \n %A = полное название дня недели, \n %b = аббр. месяца, \n %B = month name - название месяца, \n %c = дата и время, \n %d = число (01-31), \n %H = часов (24), \n %I = часов (12),\n %M = минут,\n %m = месяц(12),\n %p = a";
+		["A_ExtenalSearch"]	= "Другие аддоны могут использовать BeanCounter-поиск, который покажет BeanCounter-интерфейс. Например, это позволит BeanCounter-у показывать при просмотре Оценщика (Appraiser)";
+		["A_MailInvoiceTimeout"]	= "Время ожидания ответа от сервера накладной. Накладная -  кто, что, как почты дома Аукциона.";
+		["A_MailRecolor"]	= "BeanCounter читает всю почту от Аукционного дома. Этот параметр скажет BeanCounter'у, как пользователь хочет изменить цвет сообщений, чтобы было видно что они не прочитаны.";
+		["HelpGuiItemBox"]	= "Поместите предмет в ячейку для поиска.";
+		["Q_DateString"]	= "Используемый формат даты?";
+		["Q_DateStringCommands"]	= "Приемлемые Команды Даты?";
+		["Q_ExtenalSearch"]	= "Позволить сторонним аддонам использовать BeanCounter?";
+		["Q_MailInvoiceTimeout"]	= "Сколько задержка почты?";
+		["Q_MailRecolor"]	= "Как перекрашивать?";
+
 		-- Section: Mail
+		["MailAllianceAuctionHouse"]	= "Аукцион Альянса";
+		["MailAuctionCancelledSubject"]	= "Аукцион отменен";
+		["MailAuctionExpiredSubject"]	= "Аукцион просрочен";
+		["MailAuctionSuccessfulSubject"]	= "Аукцион успешно состоялся";
+		["MailAuctionWonSubject"]	= "Аукцион выигран";
 		["MailHordeAuctionHouse"]	= "Аукцион Орды";
+		["MailNeutralAuctionHouse"]	= "Аукцион Черной Воды";
+		["MailOutbidOnSubject"]	= "Ставка принята";
+		["MailSalePendingOnSubject"]	= "Продажа в течение";
+
+		-- Section: Tooltip Messages
+		["TTDateString"]	= "Введите формат, в котором Вы хотели бы видеть отображаемую дату. По умолчанию %c";
+		["TTDateStringExample"]	= "Отображает каким именно образом будет выглядеть Ваш формат даты.";
+		["TTExtenalSearch"]	= "Когда производится поиск в другом аддоне, BeanCounter будет также отображать поиск для этого предмета.";
+		["TTMailInvoiceTimeout"]	= "Выбор того, насколько долго BeanCounter будет пытаться получить входящую почту с сервера до того как прекратит попытки. Ниже == быстрее, но велик шанс потерять часть данных, выше == медленнее, но увеличивает шансы получить данные даже если почтовый сервер слишком занят.";
+		["TTMailRecolor"]	= "Выберите каким именно образом почта будет появляться после того, как BeanCounter просканирует почтовый ящик.";
 
 		-- Section: User Interface
+		["UiAddonTitle"]	= "BeanCounter: Аукционная база данных";
+		["UiAucExpired"]	= "Просроченный аук.";
+		["UiAucSuccessful"]	= "Успешный аук.";
 		["UiAuctions"]	= "Аукционы";
 		["UiAuctionTransaction"]	= "Аукцион";
 		["UiBids"]	= "Ставки";
 		["UiBidTransaction"]	= "Ставка";
 		["UiBuyerSellerHeader"]	= "Покупатель/Продавец";
 		["UiBuyTransaction"]	= "Купить";
+		["UiClassicCheckBox"]	= "Показать прошлые данные";
+		["UiData"]	= "Данные";
 		["UiDateHeader"]	= "Дата";
 		["UiDepositTransaction"]	= "Депозит";
+		["UiDone"]	= "Завершить";
 		["UiExactNameSearch"]	= "Поиск точного имени";
+		["UiFailedAuctions"]	= "Неудавщийся аукционы";
+		["UiFee"]	= "Оплата";
 		["UiNameHeader"]	= "Предмет";
-		["UiNetHeader"]	= "Net";
-		["UiNetPerHeader"]	= "Net за";
+		["UiNetHeader"]	= "Сеть";
+		["UiNetPerHeader"]	= "Сеть за";
+		["UiOutbid"]	= "Выкуп";
+		["UiOutbids"]	= "Выкупы";
 		["UiPriceHeader"]	= "Цена";
+		["UiPriceper"]	= "Цена/За";
 		["UiPricePerHeader"]	= "Цена Заголовка";
 		["UiPurchases"]	= "Покупок";
 		["UiQuantityHeader"]	= "Кол-во";
@@ -605,13 +700,47 @@ BeanCounterLocalizations = {
 		["UiSearch"]	= "Поиск";
 		["UiSearchForLabel"]	= "Поиск:";
 		["UiSellTransaction"]	= "Продажа";
+		["UiServer"]	= "Сервер";
 		["UiTransactions"]	= "Переводы";
 		["UiTransactionsLabel"]	= "Переводы:";
 		["UiTransactionTypeHeader"]	= "Тип";
+		["UiWealth"]	= "Состояние";
+		["UiWononBid"]	= "Выиграно по ставке";
+		["UiWononBuyout"]	= "Выиграно по выкупу";
 
 	};
 
 	zhCN = {
+
+		-- Section: Config Text
+		["C_BeanCounterConfig"]	= "BeanCounter 设置";
+		["C_BeanCounterOptions"]	= "BeanCounter 选项";
+		["C_DateString"]	= "|CCFFFCC00 使用的日期格式:";
+		["C_DateStringExample"]	= "|CCFFFCC00 示例日期:";
+		["C_ExtenalSearch"]	= "允许外部插件使用BeanCounter的搜索?";
+		["C_MailInvoiceTimeout"]	= "邮箱通知延迟=%d秒";
+		["C_MailRecolor"]	= "邮箱上色方法";
+
+		-- Section: Generic Strings
+		["NoRe-Color"]	= "无上色";
+		["off"]	= "关闭";
+		["on"]	= "开启";
+		["Re-ColorIcons"]	= "为图标上色";
+		["Re-ColorIconsandText"]	= "为图标和文字上色";
+		["Re-ColorText"]	= "为文字上色";
+
+		-- Section: Help Text
+		["A_DateString"]	= "控制BeanCounter的GUI如何显示日期.命令以%作为前缀,并且可以混用多个命令与文字.例如%a==%X将显示Wed==21:34:21";
+		["A_DateStringCommands"]	= "命令:\n %a = 缩写的日期名,\n %A = 日期名, \n %b = 缩写的月名, \n %B = 月名,\n %c = 日期和时间, \n %d = 日 (01-31),\n %H = 小时 (24), \n %I = 小时 (12),\n %M = 分, \n %m = 月,\n %p = 每";
+		["A_ExtenalSearch"]	= "其他插件可以请求于BeanCounter的GUI内显示一件物品的搜索结果. 例如 此允许BeanCounter显示你在Appraiser查找的物品.";
+		["A_MailInvoiceTimeout"]	= "BeanCounter将等待的服务器的通知请求的时间.通知是一个记载了谁,什么,多少钱的拍卖行邮件.";
+		["A_MailRecolor"]	= "BeanCounter读取来自拍卖行的所有邮件,此选项告诉BeanCounter如何将未读邮件上色以便用户阅读.";
+		["HelpGuiItemBox"]	= "拖入物品以开始搜索";
+		["Q_DateString"]	= "使用日期格式?";
+		["Q_DateStringCommands"]	= "可接受日期命令?";
+		["Q_ExtenalSearch"]	= "允许外部插件使用BeanCounter?";
+		["Q_MailInvoiceTimeout"]	= "什么是邮件通知延迟?";
+		["Q_MailRecolor"]	= "什么是上色?";
 
 		-- Section: Mail
 		["MailAllianceAuctionHouse"]	= "联盟拍卖行";
@@ -620,22 +749,42 @@ BeanCounterLocalizations = {
 		["MailAuctionSuccessfulSubject"]	= "拍卖成功";
 		["MailAuctionWonSubject"]	= "竞拍获胜";
 		["MailHordeAuctionHouse"]	= "部落拍卖行";
+		["MailNeutralAuctionHouse"]	= "黑水拍卖行";
 		["MailOutbidOnSubject"]	= "出价被压过";
+		["MailSalePendingOnSubject"]	= "竞买等待中";
+
+		-- Section: Tooltip Messages
+		["TTDateString"]	= "键入你期望的日期显示格式.默认为%c";
+		["TTDateStringExample"]	= "显示一个示例的日期";
+		["TTExtenalSearch"]	= "当在其他插件中键入一个搜索,BeanCounter也将显示一个搜索结果";
+		["TTMailInvoiceTimeout"]	= "选择BeanCounter将尝试从服务器获取一个邮件通知的时间.低==更快但较高几率丢失数据,高==更慢但可提高获取数据的几率(如果邮件服务器非常忙).";
+		["TTMailRecolor"]	= "选择BeanCounter扫描邮箱后邮件的显示方式.";
 
 		-- Section: User Interface
+		["UiAddonTitle"]	= "BeanCounter: 拍卖历史数据库";
+		["UiAucExpired"]	= "拍卖过期";
+		["UiAucSuccessful"]	= "拍卖成功";
 		["UiAuctions"]	= "拍卖";
 		["UiAuctionTransaction"]	= "拍卖";
 		["UiBids"]	= "出价";
 		["UiBidTransaction"]	= "竞标";
 		["UiBuyerSellerHeader"]	= "买主/卖主";
 		["UiBuyTransaction"]	= "购买";
+		["UiClassicCheckBox"]	= "显示BC传统数据";
+		["UiData"]	= "数据";
 		["UiDateHeader"]	= "日期";
 		["UiDepositTransaction"]	= "保管费";
+		["UiDone"]	= "完成";
 		["UiExactNameSearch"]	= "确切名字搜索";
+		["UiFailedAuctions"]	= "已失败的拍卖";
+		["UiFee"]	= "费用";
 		["UiNameHeader"]	= "物品";
 		["UiNetHeader"]	= "净利";
 		["UiNetPerHeader"]	= "单件净利";
+		["UiOutbid"]	= "最高出价";
+		["UiOutbids"]	= "最高出价";
 		["UiPriceHeader"]	= "价格";
+		["UiPriceper"]	= "价格/单件";
 		["UiPricePerHeader"]	= "单件价格";
 		["UiPurchases"]	= "购买";
 		["UiQuantityHeader"]	= "数量";
@@ -643,13 +792,42 @@ BeanCounterLocalizations = {
 		["UiSearch"]	= "搜索";
 		["UiSearchForLabel"]	= "搜索";
 		["UiSellTransaction"]	= "出售";
+		["UiServer"]	= "服务器";
 		["UiTransactions"]	= "交易";
 		["UiTransactionsLabel"]	= "交易";
 		["UiTransactionTypeHeader"]	= "类型";
+		["UiWealth"]	= "大量";
+		["UiWononBid"]	= "竞拍获胜";
+		["UiWononBuyout"]	= "一口价获胜";
 
 	};
 
 	zhTW = {
+
+		-- Section: Config Text
+		["C_BeanCounterConfig"]	= "拍賣記錄器設定";
+		["C_BeanCounterOptions"]	= "拍賣記錄器選項";
+		["C_DateString"]	= "|CCFFFCC00 資料格式使用";
+		["C_DateStringExample"]	= "|CCFFFCC00 範例資料";
+		["C_ExtenalSearch"]	= "允許其他addons使用拍賣記錄器的搜尋？";
+		["C_MailInvoiceTimeout"]	= "郵件通知到期 = %d 秒";
+		["C_MailRecolor"]	= "信件重新上色方法";
+
+		-- Section: Generic Strings
+		["NoRe-Color"]	= "不重新上色";
+		["off"]	= "關閉";
+		["on"]	= "開啟";
+		["Re-ColorIcons"]	= "圖標重新上色";
+		["Re-ColorIconsandText"]	= "圖標和文字重新上色";
+		["Re-ColorText"]	= "文字重新上色";
+
+		-- Section: Help Text
+		["HelpGuiItemBox"]	= "將物品丟入格中搜尋";
+		["Q_DateString"]	= "要使用的資料格式？";
+		["Q_DateStringCommands"]	= "接受資料指令？";
+		["Q_ExtenalSearch"]	= "允許外部addons使用拍賣記錄器？";
+		["Q_MailInvoiceTimeout"]	= "什麼是郵件通知逾時？";
+		["Q_MailRecolor"]	= "什麼是重新上色？";
 
 		-- Section: Mail
 		["MailAllianceAuctionHouse"]	= "聯盟拍賣場";
@@ -659,21 +837,39 @@ BeanCounterLocalizations = {
 		["MailAuctionWonSubject"]	= "得標";
 		["MailHordeAuctionHouse"]	= "部落拍賣場";
 		["MailOutbidOnSubject"]	= "出價失敗";
+		["MailSalePendingOnSubject"]	= "出價等待中";
+
+		-- Section: Tooltip Messages
+		["TTDateString"]	= "請輸入資料欄位想使用的格式. 預設為 %c";
+		["TTDateStringExample"]	= "顯示現在資料格式範例";
+		["TTExtenalSearch"]	= "當從其他addon輸入搜尋時，拍賣記錄野將顯示物品搜尋結果";
+		["TTMailRecolor"]	= "選擇交易拍賣在掃瞄信箱後如何顯示郵件";
 
 		-- Section: User Interface
+		["UiAddonTitle"]	= "拍賣記錄器：拍賣交易歷史資料庫";
+		["UiAucExpired"]	= "拍賣逾時";
+		["UiAucSuccessful"]	= "拍賣成功！";
 		["UiAuctions"]	= "拍賣場";
 		["UiAuctionTransaction"]	= "拍賣";
 		["UiBids"]	= "出價";
 		["UiBidTransaction"]	= "出價";
 		["UiBuyerSellerHeader"]	= "買方/賣方";
 		["UiBuyTransaction"]	= "買";
+		["UiClassicCheckBox"]	= "顯示拍賣交易的傳統資料";
+		["UiData"]	= "資料";
 		["UiDateHeader"]	= "日期";
 		["UiDepositTransaction"]	= "保管費";
+		["UiDone"]	= "完成";
 		["UiExactNameSearch"]	= "完整名稱比對";
+		["UiFailedAuctions"]	= "失敗的拍賣";
+		["UiFee"]	= "費用";
 		["UiNameHeader"]	= "物品";
 		["UiNetHeader"]	= "淨利";
 		["UiNetPerHeader"]	= "單件淨利";
+		["UiOutbid"]	= "超過出價";
+		["UiOutbids"]	= "超過出價";
 		["UiPriceHeader"]	= "價格";
+		["UiPriceper"]	= "單一價格";
 		["UiPricePerHeader"]	= "價格標題";
 		["UiPurchases"]	= "購買";
 		["UiQuantityHeader"]	= "數量";
@@ -681,9 +877,13 @@ BeanCounterLocalizations = {
 		["UiSearch"]	= "搜尋";
 		["UiSearchForLabel"]	= "搜尋";
 		["UiSellTransaction"]	= "出售";
+		["UiServer"]	= "伺服器";
 		["UiTransactions"]	= "交易";
 		["UiTransactionsLabel"]	= "交易";
 		["UiTransactionTypeHeader"]	= "類別";
+		["UiWealth"]	= "財產";
+		["UiWononBid"]	= "成功出價";
+		["UiWononBuyout"]	= "成功直購";
 
 	};
 
