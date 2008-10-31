0563a79eedd98c52afea8076676f9b5998f5e29a
kandoko <kandoko@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2008-10-31 20:07:34 +0000

    Starts AUWC-9 Tags the strings needing localization with _TRANS(), these will need to be converted into localizer appropriate keys.
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@3737 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- Auc-Util-AHWindowControl/Auc-Util-AHWindowControl.lua
+++ Auc-Util-AHWindowControl/Auc-Util-AHWindowControl.lua
@@ -37,7 +37,7 @@ local lib, parent, private = AucAdvanced.NewModule(libType, libName)
 
 if not lib then return end
 
-local print,decode,_,_,replicate,empty,get,set,default,debugPrint,fill = AucAdvanced.GetModuleLocals()
+local print,decode,_,_,replicate,empty,get,set,default,debugPrint,fill, _TRANS = AucAdvanced.GetModuleLocals()
 
 lib.Private = private
 
@@ -91,55 +91,55 @@ function private.SetupConfigGui(gui)
 	--Setup Tab for Mover functions
 	id = gui:AddTab(libName)
 	gui:MakeScrollable(id)
-	gui:AddControl(id, "Header",     0,    "Window Movement Options")
-	gui:AddControl(id, "Checkbox",   0, 1,  "util.mover.activated", "Allow the auction frame to be movable?")
-	gui:AddTip(id, "Ticking this box will enable the ability to relocate the auction frame")
+	gui:AddControl(id, "Header",     0,    _TRANS('AHWC_Interface_WindowMovementOptions') ) --"Window Movement Options"
+	gui:AddControl(id, "Checkbox",   0, 1,  "util.mover.activated",  _TRANS('AHWC_Interface_Allowmovable') ) --"Allow the auction frame to be movable?"
+	gui:AddTip(id, _TRANS('AHWC_HelpTooltip_Allowmovable') ) --"Ticking this box will enable the ability to relocate the auction frame"
 	gui:AddHelp(id, "what is AHWindowControl",
-		"What is this utility?",
-		"This utility allows you to drag and relocate the auction frame for this play session. Just click and move where you desire. It also alows you to protect the Auction House from closing when opening certain Blizzard windows.")
-	gui:AddControl(id, "Checkbox",   0, 1,  "util.mover.rememberlastpos", "Remember last known window position?")
-	gui:AddTip(id, "If this box is checked, the auction frame will reopen in the last location it was moved to.")
+		_TRANS('AHWC_Help_whatisthis'),--"What is this utility?"
+		_TRANS'AHWC_Help_whatisthisAnswer')--This utility allows you to drag and relocate the auction frame for this play session. Just click and move where you desire. It also alows you to protect the Auction House from closing when opening certain Blizzard windows."
+	gui:AddControl(id, "Checkbox",   0, 1,  "util.mover.rememberlastpos", _TRANS('AHWC_Interface_remberlastposition') ) --"Remember last known window position?"
+	gui:AddTip(id, _TRANS('AHWC_HelpToolTip_remberlastposition') ) --"If this box is checked, the auction frame will reopen in the last location it was moved to."
 	gui:AddHelp(id, "what is remeberpos",
-		"Remember last known window position?",
-		"This will remember the auction frame's last position and re-apply it each session.")
+		_TRANS('AHWC_Help_remberlastposition'), --"Remember last known window position?"
+		_TRANS('AHWC_Help_remberlastpositionAnswer') ) --"This will remember the auction frame's last position and re-apply it each session."
 
 	--Window Protection
-	gui:AddControl(id, "Header", 0,	"Window Protection Options")
-	gui:AddControl(id, "Subhead", 0, "Protect the Auction House window:")
+	gui:AddControl(id, "Header", 0,	_TRANS("Window Protection Options") )
+	gui:AddControl(id, "Subhead", 0, _TRANS("Protect the Auction House window:") )
 	--Note the function reference in the place of the setting name.  See changes in getter, setter, and getDefault to accomodate this.
 	gui:AddControl(id, "Selectbox", 0, 1, {
-		{1, "Never"},
-		{2, "Always"},
-		{3, "When Scanning"}
-	}, "util.protectwindow.protectwindow", "Prevent other windows from closing the Auction House window.")
-	gui:AddTip(id, "This will prevent other windows from closing the Auction House window when you open them, according to your settings.")
+		{1, _TRANS("Never") },
+		{2, _TRANS("Always") },
+		{3, _TRANS("When Scanning") }
+	}, "util.protectwindow.protectwindow", _TRANS("Prevent other windows from closing the Auction House window.") )
+	gui:AddTip(id, _TRANS("This will prevent other windows from closing the Auction House window when you open them, according to your settings.") )
 			
-	gui:AddControl(id, "Checkbox", 0, 1, "util.protectwindow.processprotect", "Check this to protect the window until processing is done.")
-	gui:AddTip(id, "This option allows you to extend protection from the end of the scan until processing is done.")
+	gui:AddControl(id, "Checkbox", 0, 1, "util.protectwindow.processprotect", _TRANS("Check this to protect the window until processing is done.") )
+	gui:AddTip(id, _TRANS("This option allows you to extend protection from the end of the scan until processing is done.") )
 	gui:AddHelp(id, "What is ProtectWindow",
-		"What does Protecting the AH Window do?",
-		"The Auction House window is normally closed when you open other windows, such as the Social window, the Quest Log, or your profession windows.  This option allows it to remain open, behind those other windows.")
+		_TRANS("What does Protecting the AH Window do?"),
+		_TRANS("The Auction House window is normally closed when you open other windows, such as the Social window, the Quest Log, or your profession windows.  This option allows it to remain open, behind those other windows.") )
 	
 	--AuctionFrame Scale
 	gui:AddControl(id, "Header", 0, "") --Spacer for options
-	gui:AddControl(id, "Header", 0,	"Window Size Options")
-	gui:AddControl(id, "NumeriSlider", 0, 1, "util.ahwindowcontrol.auctionscale",    0.5, 2, 0.1, "Auction House Scale")
-	gui:AddTip(id, "This option allows you to adjust the overall size of the Auction House window. Default is 1.")
+	gui:AddControl(id, "Header", 0,	_TRANS("Window Size Options") )
+	gui:AddControl(id, "NumeriSlider", 0, 1, "util.ahwindowcontrol.auctionscale",    0.5, 2, 0.1, _TRANS("Auction House Scale") )
+	gui:AddTip(id, _TRANS("This option allows you to adjust the overall size of the Auction House window. Default is 1.") )
 	gui:AddHelp(id, "what is Auction House Scale",
-			"Auction House Scale?",
-			"The Auction House scale slider adjusts the overall size of the entire Auction House window. The default size is 1.")
+			_TRANS("Auction House Scale?"),
+			_TRANS("The Auction House scale slider adjusts the overall size of the entire Auction House window. The default size is 1.") )
 	--CompactUI
-	gui:AddControl(id, "NumeriSlider", 0, 1, "util.ahwindowcontrol.compactuiscale",    -5, 5, 0.2, "CompactUI Font Scale")
-	gui:AddTip(id, "This option allows you to adjust the text size of the CompactUI on the Browse tab. The default size is 0.")
+	gui:AddControl(id, "NumeriSlider", 0, 1, "util.ahwindowcontrol.compactuiscale",    -5, 5, 0.2, _TRANS("CompactUI Font Scale") )
+	gui:AddTip(id, _TRANS("This option allows you to adjust the text size of the CompactUI on the Browse tab. The default size is 0.") )
 	gui:AddHelp(id, "what is CompactUI Font Scale",
-			"CompactUI Font Scale?",
-			"The CompactUI Font Scale slider adjusts the text size displayed in AucAdvance CompactUI option in the Browse Tab. The default size is 0.")
+			_TRANS("CompactUI Font Scale?"),
+			_TRANS("The CompactUI Font Scale slider adjusts the text size displayed in AucAdvance CompactUI option in the Browse Tab. The default size is 0.") )
 	--SearchUI
-	gui:AddControl(id, "NumeriSlider", 0, 1, "util.ahwindowcontrol.searchuiscale",     0.5, 2, 0.1, "SearchUI Scale")
-	gui:AddTip(id, "This option allows you to adjust the overall size of the non auction house SearchUI window. The default size is 1.")
+	gui:AddControl(id, "NumeriSlider", 0, 1, "util.ahwindowcontrol.searchuiscale",     0.5, 2, 0.1, _TRANS("SearchUI Scale") )
+	gui:AddTip(id, _TRANS("This option allows you to adjust the overall size of the non auction house SearchUI window. The default size is 1.") )
 	gui:AddHelp(id, "what is SearchUI Scale",
-			"SearchUI Scale?",
-			"The SearchUI scale slider adjusts the overall size of the non auction house SearchUI window. The default size is 1.")
+			_TRANS("SearchUI Scale?"),
+			_TRANS("The SearchUI scale slider adjusts the overall size of the non auction house SearchUI window. The default size is 1.") )
 end
 
 
