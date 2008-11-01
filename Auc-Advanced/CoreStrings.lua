41dc6f1427eea74af8b581530b72d96924928eca
kandoko <kandoko@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2008-11-01 15:48:45 +0000

    Updates ADV-365 CoreStrings.lua adds localizer produced strings for AHWC
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@3742 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- Auc-Advanced/CoreStrings.lua
+++ Auc-Advanced/CoreStrings.lua
@@ -32,5 +32,66 @@
 
 AuctioneerLocalizations = {
 
+	deDE = {
+
+		-- Section: Help Text
+		["AHWC_Help_RemberLastPosition"]	= "letzte Fenster Position merken";
+		["AHWC_Help_RemberLastPositionAnswer"]	= "Ist diese Option aktiviert, wird das Auktionsfenster sich an der letzt gemerkten Stelle wieder öffnen";
+		["AHWC_Help_whatisthis"]	= "Was für ein Hilfsmittel ist das?";
+		["AHWC_Help_whatisthisAnswer"]	= "Dieses Hilfsmittel erlaubt euch das Auktionsfenster zu ziehen und für diese Spielzeit an eine andere Stelle zu bewegen. Nutz diese Option und bewegt das Fenster wohin Ihr es möchtet. Ebenfalls wird dann das Auktionshausfenster nicht mehr geschlossen, wenn Ihr andere Fenster öffnet.";
+
+		-- Section: HelpTooltip
+		["AHWC_HelpToolTip_RemberLastPosition"]	= "Wenn diese Option angewählt ist, wird sich das Auktionsfenster wieder da öffnen wo es zuletzt  hin verschoben wurde.";
+
+		-- Section: Interface
+		["AHWC_HelpTooltip_AllowMovable"]	= "Diese Option aktivieren ermöglicht das Auktionen Fenster zu bewegen.";
+		["AHWC_Interface_AllowMovable"]	= "Erlaubt es das Auktionsfenster zu bewegen.";
+		["AHWC_Interface_RemberLastPosition"]	= "Soll die letzte Position gespeichert werden?";
+		["AHWC_Interface_WindowMovementOptions"]	= "Einstellungen für beweglichkeit der Fenster.";
+
+	};
+
+	enUS = {
+
+		-- Section: Help Text
+		["AHWC_Help_AuctionHouseScale"]	= "Auction House Scale?";
+		["AHWC_Help_AuctionHouseScaleAnswer"]	= "The Auction House scale slider adjusts the overall size of the entire Auction House window. The default size is 1.";
+		["AHWC_Help_CompactUIFontScale"]	= "CompactUI Font Scale?";
+		["AHWC_Help_CompactUIFontScaleAnswer"]	= "The CompactUI Font Scale slider adjusts the text size displayed in AucAdvance CompactUI option in the Browse Tab. The default size is 0.";
+		["AHWC_Help_ProtectWindow"]	= "What does Protecting the AH Window do?";
+		["AHWC_Help_ProtectWindowAnswer"]	= "The Auction House window is normally closed when you open other windows, such as the Social window, the Quest Log, or your profession windows. This option allows it to remain open, behind those other windows.";
+		["AHWC_Help_RemberLastPosition"]	= "Remember last known window position?";
+		["AHWC_Help_RemberLastPositionAnswer"]	= "If this box is checked, the auction frame will reopen in the last location it was moved to.";
+		["AHWC_Help_SearchUIScale"]	= "SearchUI Scale?";
+		["AHWC_Help_SearchUIScaleAnswer"]	= "The SearchUI scale slider adjusts the overall size of the non auction house SearchUI window. The default size is 1.";
+		["AHWC_Help_whatisthis"]	= "What is this utility?";
+		["AHWC_Help_whatisthisAnswer"]	= "This utility allows you to drag and relocate the auction frame for this play session. Just click and move where you desire. It also alows you to protect the Auction House from closing when opening certain Blizzard windows.";
+
+		-- Section: HelpTooltip
+		["AHWC_HelpToolTip_AuctionHouseScale"]	= "This option allows you to adjust the overall size of the Auction House window. Default is 1.";
+		["AHWC_HelpTooltip_CompactUIFontScale"]	= "This option allows you to adjust the text size of the CompactUI on the Browse tab. The default size is 0.";
+		["AHWC_HelpToolTip_PreventClosingAuctionHouse"]	= "This will prevent other windows from closing the Auction House window when you open them, according to your settings";
+		["AHWC_HelpToolTip_RemberLastPosition"]	= "If this box is checked, the auction frame will reopen in the last location it was moved to.";
+		["AHWC_HelpTooltip_SearchUIScale"]	= "This option allows you to adjust the overall size of the non auction house SearchUI window. The default size is 1.";
+
+		-- Section: Interface
+		["AHWC_HelpTooltip_AllowMovable"]	= "Ticking this box will enable the ability to relocate the auction frame";
+		["AHWC_HelpToolTip_ProtectProcessing"]	= "This option allows you to extend protection from the end of the scan until processing is done.";
+		["AHWC_Interface_AllowMovable"]	= "Allow the auction frame to be movable";
+		["AHWC_Interface_Always"]	= "Always";
+		["AHWC_Interface_AuctionHouseScale"]	= "Auction House Scale";
+		["AHWC_Interface_CompactUIFontScale"]	= "CompactUI Font Scale";
+		["AHWC_Interface_Never"]	= "Never";
+		["AHWC_Interface_PreventClosingAuctionHouse"]	= "Prevent other windows from closing the Auction House window.";
+		["AHWC_Interface_ProtectAuctionWindow"]	= "Protect the Auction House window:";
+		["AHWC_Interface_ProtectProcessing"]	= "Check this to protect the window until processing is done.";
+		["AHWC_Interface_RemberLastPosition"]	= "Remember last known window position?";
+		["AHWC_Interface_Scanning"]	= "When Scanning";
+		["AHWC_Interface_SearchUIScale"]	= "SearchUI Scale";
+		["AHWC_Interface_WindowMovementOptions"]	= "Window Movement Options";
+		["AHWC_Interface_WindowProtectionOptions"]	= "Window Protection Options";
+		["AHWC_Interface_WindowSizeOptions"]	= "Window Size Options";
+
+	};
 
 }
