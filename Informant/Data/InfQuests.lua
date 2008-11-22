d24df304c65efd5bd886814dba227915ef6d51d6
Hirsute <Hirsute@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2008-11-22 18:06:57 +0000

    INF-37: Found and fixed more inaccurate prices
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@3863 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- Informant/Data/InfQuests.lua
+++ Informant/Data/InfQuests.lua
@@ -1,7 +1,7 @@
 --[[
 	Up-to-date list of quest items and rewards for informant.
 	File courtesy of http://www.wowhead.com/
-	File generated at: 2008-11-22 03:38:46 Z
+	File generated at: 2008-11-22 12:39:52 Z
 --]]
 Informant.SetQuestStarts({
 	[1307]='123',
