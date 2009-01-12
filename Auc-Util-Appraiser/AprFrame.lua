d5f2ceea1e6db8b58690ecb9ab306b5493fdd09f
kandoko <kandoko@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2009-01-12 17:36:57 +0000

    Fixes APPR-243 Removes scrollframe reset code since function is now handled internally by configator.
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@3978 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- Auc-Util-Appraiser/AprFrame.lua
+++ Auc-Util-Appraiser/AprFrame.lua
@@ -394,11 +394,6 @@ function private.CreateFrames()
 		frame.refresh:Enable()
 		frame.switchUI:Enable()
 		frame.imageview.sheet:SetData(data, style)
-		--reset scroll position if new items list is too short to show
-		if  not frame.imageview.sheet.rows[1][1]:IsShown() then
-			frame.imageview.sheet.panel:ScrollToCoords(0,0)
-		end
-
 	end
 
 	function frame.SetPriceColor(itemID, count, requiredBid, buyoutPrice, rDef, gDef, bDef)
