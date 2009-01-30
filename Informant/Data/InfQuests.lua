4b6f345f312b92e443dfa75411d13fb4ca797517
Hirsute <Hirsute@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2009-01-30 18:41:25 +0000

    [INF-54] Removes the trailing whitespace that is borking localization of the additional field in some cases.
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@4036 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- Informant/Data/InfQuests.lua
+++ Informant/Data/InfQuests.lua
@@ -1,7 +1,7 @@
 --[[
 	Up-to-date list of quest items and rewards for informant.
 	File courtesy of http://www.wowhead.com/
-	File generated at: 2009-01-30 00:36:07 Z
+	File generated at: 2009-01-30 14:32:53 Z
 --]]
 Informant.SetQuestStarts({
 	[1307]='123',
