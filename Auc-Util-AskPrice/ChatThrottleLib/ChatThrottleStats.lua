a3251f0efe8a3045eee4a93c1b125b6531554576
aesir <aesir@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2009-04-14 19:33:21 +0000

    [ASKP-29] Update Auc-Util-AskPrice ChatThrottleLib package to v20.
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@4154 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- Auc-Util-AskPrice/ChatThrottleLib/ChatThrottleStats.lua
+++ Auc-Util-AskPrice/ChatThrottleLib/ChatThrottleStats.lua
@@ -17,32 +17,32 @@ local lasttime = GetTime();
 
 CTLStats:SetScript("OnUpdate", function()
 	local now = GetTime()
-
+	
 	if(now - lasttime > 1) then
-
+		
 		ChatThrottleLib:UpdateAvail();		-- NOTE THAT THIS NORMALLY DOES NOT GET CALLED PERIODICALLY. Disable this for final testing!
-
+	
 		local sent = 0;
 		for _,Prio in pairs(ChatThrottleLib.Prio) do
 			sent = sent + Prio.nTotalSent;
 		end
-
-		CTLStatsText:SetText(format("%4.0f cps via lib (%4.0f bytes avail), %4.0f cps bypassed lib",
-			(sent-lastsent) / (now-lasttime),
+		
+		CTLStatsText:SetText(format("%4.0f cps via lib (%4.0f bytes avail), %4.0f cps bypassed lib", 
+			(sent-lastsent) / (now-lasttime), 
 			ChatThrottleLib.avail,
 			(ChatThrottleLib.nBypass-lastbypass) / (now-lasttime)
 		));
-
+		
 		lasttime=now;
 		lastsent=sent;
 		lastbypass=ChatThrottleLib.nBypass;
-
+		
 	end
-
+	
 	if(ChatThrottleLib.bChoking) then
 		CTLStatsText:SetTextColor(1,0.2,0.2);
-	else
+	else	
 		CTLStatsText:SetTextColor(0.9,0.9,1);
 	end
-
+	
 end);
