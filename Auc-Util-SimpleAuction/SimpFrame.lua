e8bdf37934435c614d5de889256bd2115079d9a1
prowell <prowell@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2010-03-27 21:24:23 +0000

    Fixes [ADV-527]  Prior commits had function parameters in the wrong order.
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@4700 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- Auc-Util-SimpleAuction/SimpFrame.lua
+++ Auc-Util-SimpleAuction/SimpFrame.lua
@@ -1248,7 +1248,7 @@ function private.CreateFrames()
 	frame.scanbutton:SetPoint("LEFT", "AuctionFrameMoneyFrame", "RIGHT", 5,0)
 	frame.scanbutton:SetScript("OnClick", function()
 		if not AucAdvanced.Scan.IsScanning() then
-			AucAdvanced.Scan.StartScan("", "", "", AuctionFrameBrowse.selectedClassIndex, AuctionFrameBrowse.selectedSubclassIndex, AuctionFrameBrowse.selectedInvtypeIndex, nil, nil)
+			AucAdvanced.Scan.StartScan("", "", "", AuctionFrameBrowse.selectedInvtypeIndex, AuctionFrameBrowse.selectedClassIndex, AuctionFrameBrowse.selectedSubclassIndex,  nil, nil)
 		end
 	end)
 
