ded10370a99aef75a5d1299d70a33b05f2aae3dc
brykrys <brykrys@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2010-11-01 14:04:07 +0000

    Completes [SMPL-47] <internal> Minor tweaks for recent CorePost changes
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@4993 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- Auc-Util-SimpleAuction/SimpFrame.lua
+++ Auc-Util-SimpleAuction/SimpFrame.lua
@@ -624,7 +624,7 @@ function private.LoadItemLink(itemLink, size)
 			if reason == "Damaged" then
 				aucPrint(itemLink.." is damaged: you may be able to post it after repairing it")
 			else
-				aucPrint(itemLink.." cannot be posted: "..(AucAdvanced.Post.ErrorText[reason] or "Unknown Reason"))
+				aucPrint(itemLink.." cannot be posted: "..AucAdvanced.Post.GetErrorText(reason))
 			end
 			return private.LoadItemLink()
 		end
@@ -806,8 +806,7 @@ function private.PostAuction()
 	if success then
 		aucPrint("Posting "..number.." stacks of "..stack.."x "..link.." at Bid:"..coins(bid)..", BO:"..coins(buy).." for "..duration.."h")
 	else
-		reason = AucAdvanced.Post.ErrorText[reason] or "Unknown Reason"
-		aucPrint("Posting Failed for "..link..": "..reason)
+		aucPrint("Posting Failed for "..link..": "..AucAdvanced.Post.GetErrorText(reason))
 	end
 end
 
