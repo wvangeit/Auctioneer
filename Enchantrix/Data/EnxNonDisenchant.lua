b24a4450ca37e5b983e5990634507ec15013c943
ccox <ccox@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2010-11-25 23:20:47 +0000

    part of [ENX-135] add the rest of the non-DE items I have found
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@5026 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- Enchantrix/Data/EnxNonDisenchant.lua
+++ Enchantrix/Data/EnxNonDisenchant.lua
@@ -9,10 +9,50 @@
 		We'll be gathering new information as Cataclysm develops and new items are found.
 ]]
 NonDisenchantables = {
-	["11287:0:0"] = true,
+
+	["11287:0:0"] = true,	-- enchanter created wands
 	["11288:0:0"] = true,
-	["20406:0:0"] = true,
+	["11289:0:0"] = true,
+	["11290:0:0"] = true,
+	
+	["17690:0:0"] = true,	-- frostwolf and stormpike insignias
+	["17691:0:0"] = true,
+	["17900:0:0"] = true,
+	["17901:0:0"] = true,
+	["17902:0:0"] = true,
+	["17903:0:0"] = true,
+	["17904:0:0"] = true,
+	["17905:0:0"] = true,
+	["17906:0:0"] = true,
+	["17907:0:0"] = true,
+	["17908:0:0"] = true,
+	["17909:0:0"] = true,
+	
+	["20406:0:0"] = true,	-- twilight cultists garb
 	["20407:0:0"] = true,
 	["20408:0:0"] = true,
+
+	["42943:0:0"] = true,	-- heirloom items
+	["42944:0:0"] = true,
+	["42945:0:0"] = true,
+	["42946:0:0"] = true,
+	["42947:0:0"] = true,
+	["42948:0:0"] = true,
+	["42949:0:0"] = true,
+	["42950:0:0"] = true,
+	["42951:0:0"] = true,
+	["42952:0:0"] = true,
+	["42984:0:0"] = true,
+	["42985:0:0"] = true,
+	["42991:0:0"] = true,
+	["42992:0:0"] = true,
+	["48677:0:0"] = true,
+	["48683:0:0"] = true,
+	["48685:0:0"] = true,
+	["48687:0:0"] = true,
+	["48689:0:0"] = true,
+	["48691:0:0"] = true,
+	["48716:0:0"] = true,
+	["48718:0:0"] = true,
 }
 
