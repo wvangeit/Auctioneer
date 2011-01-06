3ca3e8927f5f3a5337f17f829d83c29800a2cf69
Mascondante <Mascondante@21a0ffd4-88d5-4261-b61c-461777ffcffb>
2011-01-06 03:40:08 +0000

    Implements GLYPH-50.  Experiment adding standard header comment.
    git-svn-id: http://svn.norganna.org/auctioneer/trunk@5060 21a0ffd4-88d5-4261-b61c-461777ffcffb

--- Auc-Util-Glypher/Glypher.lua
+++ Auc-Util-Glypher/Glypher.lua
@@ -1,4 +1,33 @@
+--[[
+	Auctioneer Addon for World of Warcraft(tm).
+	Version: <%version%> (<%codename%>)
+	Revision: $Id: Glypher.lua 5060 2011-01-05 03:32:00Z Mascondante $
 
+	Glypher - Inscriptionist's 'Swiss Army Knife'
+	URL: http://auctioneeraddon.com/
+
+	License:
+		This program is free software; you can redistribute it and/or
+		modify it under the terms of the GNU General Public License
+		as published by the Free Software Foundation; either version 2
+		of the License, or (at your option) any later version.
+
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with this program(see GPL.txt); if not, write to the Free Software
+		Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+	Note:
+		This AddOn's source code is specifically designed to work with
+		World of Warcraft's interpreted AddOn system.
+		You have an implicit license to use this AddOn with these facilities
+		since that is it's designated purpose as per:
+		http://www.fsf.org/licensing/licenses/gpl-faq.html#InterpreterIncompat
+]]
 if not AucAdvanced then return end
 
 local libType, libName = "Util", "Glypher"
